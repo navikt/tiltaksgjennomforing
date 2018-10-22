65b12ff4a80eff7d19a5e14a7e063c3a86b7340e
Mats Bolstad <mbolstad@gmail.com>
2018-10-22 12:21:11 +0200

    Tester tekstendring.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">HELLO #TAG!</h1>
+          <p>Tiltaksgjennomføring</p>
         </header>
       </div>
     );
