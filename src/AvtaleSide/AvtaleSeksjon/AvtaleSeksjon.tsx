c2e5c0e9649e123cfa19e66d2e30f20c86666ef3
andrmos <mossige.a@gmail.com>
2018-11-09 14:36:44 +0100

    TAG-81: Lag oppfolging-steg

--- src/AvtaleSide/AvtaleSeksjon/AvtaleSeksjon.tsx
+++ src/AvtaleSide/AvtaleSeksjon/AvtaleSeksjon.tsx
@@ -5,12 +5,14 @@ import BekreftelseSteg from './BekreftelseSteg';
 import MalsetningSteg from './MalsetningSteg';
 import ArbeidstidSteg from './ArbeidstidSteg/ArbeidstidSteg';
 import ArbeidsoppgaverSteg from './ArbeidsoppgaverSteg';
+import OppfolgingSteg from './OppfolgingSteg';
 
 const AvtaleSeksjon = (props: AvtaleProps) => (
     <Stegvelger>
         <MalsetningSteg label={'Målsetninger'} {...props} />
         <ArbeidsoppgaverSteg label={'Arbeidsoppgaver'} {...props} />
         <ArbeidstidSteg label={'Dato og arbeidstid'} {...props} />
+        <OppfolgingSteg label={'Oppfølging'} {...props} />
         <BekreftelseSteg label={'Bekreftelse'} {...props} />
     </Stegvelger>
 );
