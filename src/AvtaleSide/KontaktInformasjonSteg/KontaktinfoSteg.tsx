665578814719e6945e9e0deafbff97c1c7238025
andrmos <mossige.a@gmail.com>
2018-11-15 11:45:49 +0100

    TAG-86: Fjern ubrukte props fra KontaktinfoSteg

--- src/AvtaleSide/KontaktInformasjonSteg/KontaktinfoSteg.tsx
+++ src/AvtaleSide/KontaktInformasjonSteg/KontaktinfoSteg.tsx
@@ -1,23 +1,10 @@
 import * as React from 'react';
 import './KontaktinfoSteg.less';
-import { EndreAvtale } from '../EndreAvtale';
-import {
-    Arbeidsgiverinfo,
-    Bedriftinfo,
-    Deltakerinfo,
-    Veilederinfo,
-} from '../avtale';
 import DeltakerinfoDel from './DeltakerinfoDel/DeltakerinfoDel';
 import ArbeidsgiverinfoDel from './ArbeidsgiverinfoDel/ArbeidsgiverinfoDel';
 import VeilederinfoDel from './VeilederinfoDel/VeilederinfoDel';
 
-type Kontaktinformasjon = Deltakerinfo &
-    Bedriftinfo &
-    Arbeidsgiverinfo &
-    Veilederinfo &
-    EndreAvtale;
-
-const KontaktinfoSteg = (props: Kontaktinformasjon) => {
+const KontaktinfoSteg = () => {
     return (
         <div className="kontaktinformasjon">
             <DeltakerinfoDel />
