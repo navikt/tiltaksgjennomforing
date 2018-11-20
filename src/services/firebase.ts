6c0c53feea807f3e7ff98bf2289c47f649756b28
andrmos <mossige.a@gmail.com>
2018-11-20 15:57:44 +0100

    Fjern firebase varsel fra console

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
 import * as moment from 'moment';
 import { Avtale } from '../Stegside/avtale';
 import DataSnapshot = firebase.database.DataSnapshot;
