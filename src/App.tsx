78e87b3be7708ecc6e6be9c56d7aa0e60bb683f9
andrmos <mossige.a@gmail.com>
2018-11-26 15:57:39 +0100

    TAG-96: Legg til intl provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import * as React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { AvtaleProvider } from './Stegside/AvtaleContext';
 import Stegside from './Stegside/Stegside';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import * as nb from 'react-intl/locale-data/nb';
+
+addLocaleData(nb);
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
-                <AvtaleProvider>
-                    <Route
-                        path="/avtale/:avtaleId/:stegPath"
-                        exact={true}
-                        component={Stegside}
-                    />
-                </AvtaleProvider>
-            </BrowserRouter>
+            <IntlProvider locale="nb">
+                <BrowserRouter>
+                    <AvtaleProvider>
+                        <Route
+                            path="/avtale/:avtaleId/:stegPath"
+                            exact={true}
+                            component={Stegside}
+                        />
+                    </AvtaleProvider>
+                </BrowserRouter>
+            </IntlProvider>
         );
     }
 }
