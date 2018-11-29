d128a3317db22f472064067401a6c36b83bd5831
andrmos <mossige.a@gmail.com>
2018-11-29 10:07:38 +0100

    TAG-109: Forenkle godkjenningsknapper komponent

--- src/Stegside/GodkjenningSteg/GodkjenningKnapper/GodkjenningKnapper.tsx
+++ src/Stegside/GodkjenningSteg/GodkjenningKnapper/GodkjenningKnapper.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import Innholdsboks from '../../../komponenter/Innholdsboks/Innholdsboks';
-import { SkjemaGruppe } from 'nav-frontend-skjema';
-import PanelBase from 'nav-frontend-paneler';
 import { Knapp } from 'nav-frontend-knapper';
 import { Avtale } from '../../avtale';
 
@@ -14,43 +12,29 @@ interface Props {
 
 const GodkjenningKnapper = (props: Props) => (
     <Innholdsboks>
-        <PanelBase>
-            <SkjemaGruppe
-                className={'bekreft'}
-                title={'Bekreft innhold i avtalen'}
-            >
-                <div>
-                    <Knapp
-                        disabled={props.avtale.bekreftetAvBruker}
-                        onClick={props.bekreftBrukerOnClick}
-                    >
-                        Bekreft som bruker
-                    </Knapp>
-                    {props.avtale.bekreftetAvBruker &&
-                        'Avtalen er bekreftet av bruker'}
-                </div>
-                <div>
-                    <Knapp
-                        disabled={props.avtale.bekreftetAvArbeidsgiver}
-                        onClick={props.bekreftArbeidsgiverOnClick}
-                    >
-                        Bekreft som arbeidsgiver
-                    </Knapp>
-                    {props.avtale.bekreftetAvArbeidsgiver &&
-                        'Avtalen er bekreftet av arbeidsgiver'}
-                </div>
-                <div>
-                    <Knapp
-                        disabled={props.avtale.bekreftetAvVeileder}
-                        onClick={props.bekreftVeilederOnClick}
-                    >
-                        Bekreft som NAV-veileder
-                    </Knapp>
-                    {props.avtale.bekreftetAvVeileder &&
-                        'Avtalen er bekreftet av NAV-veileder'}
-                </div>
-            </SkjemaGruppe>
-        </PanelBase>
+        <Knapp
+            disabled={props.avtale.bekreftetAvBruker}
+            onClick={props.bekreftBrukerOnClick}
+        >
+            Bekreft som bruker
+        </Knapp>
+        {props.avtale.bekreftetAvBruker && 'Avtalen er bekreftet av bruker'}
+        <Knapp
+            disabled={props.avtale.bekreftetAvArbeidsgiver}
+            onClick={props.bekreftArbeidsgiverOnClick}
+        >
+            Bekreft som arbeidsgiver
+        </Knapp>
+        {props.avtale.bekreftetAvArbeidsgiver &&
+            'Avtalen er bekreftet av arbeidsgiver'}
+        <Knapp
+            disabled={props.avtale.bekreftetAvVeileder}
+            onClick={props.bekreftVeilederOnClick}
+        >
+            Bekreft som NAV-veileder
+        </Knapp>
+        {props.avtale.bekreftetAvVeileder &&
+            'Avtalen er bekreftet av NAV-veileder'}
     </Innholdsboks>
 );
 
