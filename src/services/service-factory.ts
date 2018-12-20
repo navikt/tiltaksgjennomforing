87c251647afd90d85a691c3a4c22bb5e36f2dab0
Mats Bolstad <mbolstad@gmail.com>
2018-12-20 10:59:05 +0100

    Må kjøre npm med environment variabel REACT_APP_FIREBASE=true for å kjøre Firebase.

--- src/services/service-factory.ts
+++ src/services/service-factory.ts
@@ -3,7 +3,7 @@ import RestService from './rest-service';
 import Service from './service';
 
 export const createService = (): Service => {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_FIREBASE) {
         return new FirebaseService();
     }
     return new RestService();
