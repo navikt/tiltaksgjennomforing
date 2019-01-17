04ee9900f0818bcec9e430c0b2e349075e9979d2
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-01-17 13:33:39 +0100

    TAG-201 Legg til kopieringsfuksjonalitet

--- src/OpprettAvtale/OpprettelseFullfort/OpprettelseFullfort.tsx
+++ src/OpprettAvtale/OpprettelseFullfort/OpprettelseFullfort.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
-import { Innholdstittel, Normaltekst, Sidetittel, Element } from 'nav-frontend-typografi';
+import {
+    Innholdstittel,
+    Normaltekst,
+    Sidetittel,
+    Element,
+} from 'nav-frontend-typografi';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { RouterProps } from 'react-router';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import { pathTilKontaktinformasjonSteg } from '../../paths';
 import { Context, medContext } from '../../AvtaleContext';
 import Veilederpanel from 'nav-frontend-veilederpanel';
 import utklippstavleIkon from '../utklippstavle.svg';
 import avsjekkSirkelIkon from './avsjekk-sirkel.svg';
 
-import './OpprettelseFullfort.less'
+import './OpprettelseFullfort.less';
+import { Input } from 'nav-frontend-skjema';
 
-const OpprettelseFullfort: React.FunctionComponent<Context & RouterProps> = props => {
+const OpprettelseFullfort: React.FunctionComponent<
+    Context & RouterProps
+> = props => {
     const tilAvtalenKlikk = () => {
         props.history.push(pathTilKontaktinformasjonSteg(props.avtale.id));
     };
 
-    const hrefTilAvtaleForBrukerOgAG = `https://arbeidsgiver.nav.no/tiltaksgjennomforing/avtale/${props.avtale.id}`;
+    const hrefTilAvtaleForBrukerOgAG = `https://arbeidsgiver.nav.no/tiltaksgjennomforing/avtale/${
+        props.avtale.id
+    }`;
+
+    const inputLabel = (
+        <Element className="opprettelseFullfort__undertittel">
+            Kopier denne lenken for å dele
+        </Element>
+    );
 
     const veilederpanel = (
         <Veilederpanel
-            svg={<img className="opprettelseFullfort__ikon" src={utklippstavleIkon} />}
+            svg={
+                <img
+                    className="opprettelseFullfort__ikon"
+                    src={utklippstavleIkon}
+                    alt=""
+                />
+            }
             kompakt={true}
             type="plakat"
         >
@@ -27,16 +50,22 @@ const OpprettelseFullfort: React.FunctionComponent<Context & RouterProps> = prop
                 Dette må du gjøre videre
             </Innholdstittel>
             <Normaltekst>
-                For at arbeidsgiver og kandidat skal kunne logge seg inn og fylle ut avtalen må du sende dem lenken vist nedenfor. De kan da kunne logge seg inn i avtalen med Min ID.
+                For at arbeidsgiver og kandidat skal kunne logge seg inn og
+                fylle ut avtalen må du sende dem lenken vist nedenfor. De kan da
+                kunne logge seg inn i avtalen med Min ID.
             </Normaltekst>
-            <Element className="opprettelseFullfort__undertittel">
-                Kopier denne lenken for å dele
-            </Element>
-            <div className="opprettelseFullfort__kopierLenke-wrapper">
-                <Normaltekst>
-                    <a href={hrefTilAvtaleForBrukerOgAG} className="lenke nav-frontend-lenke">{hrefTilAvtaleForBrukerOgAG}</a>
-                </Normaltekst>
-                <Hovedknapp>Kopier lenke</Hovedknapp>
+
+            <div className="opprettelseFullfort__lenkedeling">
+                <Input
+                    label={inputLabel}
+                    className="opprettelseFullfort__inputfelt"
+                    value={hrefTilAvtaleForBrukerOgAG}
+                />
+                <CopyToClipboard text={hrefTilAvtaleForBrukerOgAG}>
+                    <Hovedknapp className="opprettelseFullfort__kopier-knapp">
+                        Kopier lenke
+                    </Hovedknapp>
+                </CopyToClipboard>
             </div>
         </Veilederpanel>
     );
@@ -44,14 +73,23 @@ const OpprettelseFullfort: React.FunctionComponent<Context & RouterProps> = prop
     return (
         <div className="opprettelseFullfort">
             <div className="opprettelseFullfort__sidetittel-wrapper">
-                <img src={avsjekkSirkelIkon} className="opprettelseFullfort__sidetittel-ikon"/>
+                <img
+                    src={avsjekkSirkelIkon}
+                    className="opprettelseFullfort__sidetittel-ikon"
+                    alt="avsjekk-ikon"
+                />
                 <Sidetittel>Avtalen ble opprettet</Sidetittel>
             </div>
             {veilederpanel}
-            <Hovedknapp onClick={tilAvtalenKlikk} className="opprettelseFullfort__knapp">
+            <Hovedknapp
+                onClick={tilAvtalenKlikk}
+                className="opprettelseFullfort__knapp"
+            >
                 Gå til avtalen
             </Hovedknapp>
-            <a className="lenke" onClick={() => props.history.goBack()}>Tilbake</a>
+            <a className="lenke" onClick={() => props.history.goBack()}>
+                Tilbake
+            </a>
         </div>
     );
 };
