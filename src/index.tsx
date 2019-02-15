bb884e6d6cf5036caabc410df2a1c4549fd6a0e0
Mats Bolstad <mbolstad@gmail.com>
2019-02-15 09:37:30 +0100

    TAG-292: Polyfill av intl for IE.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import 'babel-polyfill';
+import 'react-app-polyfill/ie9';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.less';
+
+if (!global.Intl) {
+    global.Intl = require('intl');
+}
 
 if (process.env.REACT_APP_MOCK) {
     console.log('========================================');
