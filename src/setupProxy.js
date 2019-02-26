1b03240eb0c980f29e55e5e3650197b1ee45e72d
Mats Bolstad <mbolstad@gmail.com>
2019-02-26 15:24:37 +0100

    Sjekker at URL til login og logout er definert. Viser loginn-knapp bare hvis definert for den kilden.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,25 @@
 const proxy = require('http-proxy-middleware');
 
+const erDevelopmentModus = process.env.NODE_ENV === 'development';
+
 const envProperties = {
     APIGW_URL: process.env.APIGW_URL || 'http://localhost:8080',
     APIGW_HEADER: process.env.APIGW_HEADER,
     ISSO_LOGIN_URL:
         process.env.ISSO_LOGIN_URL ||
-        '/tiltaksgjennomforing/api/local/isso-login?redirect=http://localhost:3000/tiltaksgjennomforing',
+        (erDevelopmentModus && '/tiltaksgjennomforing/api/local/isso-login?redirect=http://localhost:3000/tiltaksgjennomforing'),
     SELVBETJENING_LOGIN_URL:
         process.env.SELVBETJENING_LOGIN_URL ||
-        '/tiltaksgjennomforing/api/local/selvbetjening-login?redirect=http://localhost:3000/tiltaksgjennomforing',
+        (erDevelopmentModus && '/tiltaksgjennomforing/api/local/selvbetjening-login?redirect=http://localhost:3000/tiltaksgjennomforing'),
     LOGOUT_URL:
         process.env.LOGOUT_URL ||
-        '/tiltaksgjennomforing/api/local/logout?redirect=http://localhost:3000/tiltaksgjennomforing',
+        (erDevelopmentModus && '/tiltaksgjennomforing/api/local/logout?redirect=http://localhost:3000/tiltaksgjennomforing'),
 };
+
+if (!envProperties.LOGOUT_URL || !(envProperties.ISSO_LOGIN_URL || envProperties.SELVBETJENING_LOGIN_URL)) {
+    console.error('Må sette en variabel for innlogging og en for utlogging: LOGOUT_URL, SELVBETJENING_LOGIN_URL, ISSO_LOGIN_URL.');
+    process.exit(1);
+}
 
 console.log('Relevante miljøvariable: ', envProperties);
 
