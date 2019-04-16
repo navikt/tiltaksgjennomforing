afed8d362dcef2d7374bd23193e9edf4cfdc3340
Sindre D Løken <sindre.loken@soprasteria.com>
2019-04-16 13:46:19 +0000

    Lenkedelingmodal komponent og lenke for å åpne den

--- src/AvtaleSide/DesktopAvtaleSide/DesktopAvtaleSide.tsx
+++ src/AvtaleSide/DesktopAvtaleSide/DesktopAvtaleSide.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AvtaleStegType } from '../AvtaleSide';
 import Stegmeny from '../Stegmeny/Stegmeny';
+import Lenke from 'nav-frontend-lenker';
+import shareIkon from '../../assets/ikoner/share.svg';
+import KopierLenkeModal from '../../komponenter/modal/KopierLenkeModal';
 
 interface Props {
     avtaleSteg: AvtaleStegType;
     aktivtSteg: string;
 }
 
-const DesktopAvtaleSide: React.FunctionComponent<Props> = props => (
-    <>
-        <Stegmeny steg={props.avtaleSteg} aktivtSteg={props.aktivtSteg} />
-        <form className="avtaleside__innhold">
-            {props.avtaleSteg[props.aktivtSteg].komponent}
-        </form>
-    </>
-);
+const DesktopAvtaleSide: React.FunctionComponent<Props> = props => {
+    const [isOpen, setOpen] = useState<boolean>(false);
+    return (
+        <>
+            <Stegmeny steg={props.avtaleSteg} aktivtSteg={props.aktivtSteg} />
+            <form className="avtaleside__innhold">
+                <div className="avtaleside__lenkedeling">
+                    <Lenke onClick={() => setOpen(true)} href="#">
+                        Del lenke til avtalen <img src={shareIkon} />
+                    </Lenke>
+                </div>
+                {props.avtaleSteg[props.aktivtSteg].komponent}
+            </form>
+
+            <KopierLenkeModal
+                isOpen={isOpen}
+                lukkModal={() => setOpen(false)}
+            />
+        </>
+    );
+};
 export default DesktopAvtaleSide;
