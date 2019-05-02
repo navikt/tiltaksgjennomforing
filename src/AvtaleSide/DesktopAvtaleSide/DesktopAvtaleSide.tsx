482c3a24e270a18a61df9a14f6d54e9f5e2343db
Mats Bolstad <mbolstad@gmail.com>
2019-05-02 17:07:09 +0200

    Reverterer branch TAG-454 som feilaktig ble merget til master.

--- src/AvtaleSide/DesktopAvtaleSide/DesktopAvtaleSide.tsx
+++ src/AvtaleSide/DesktopAvtaleSide/DesktopAvtaleSide.tsx
@@ -1,42 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AvtaleStegType } from '../AvtaleSide';
 import Stegmeny from '../Stegmeny/Stegmeny';
-import Lenke from 'nav-frontend-lenker';
-import { ReactComponent as ShareIkon } from '../../assets/ikoner/share.svg';
-import KopierLenkeModal from '../../komponenter/modal/KopierLenkeModal';
 
 interface Props {
     avtaleSteg: AvtaleStegType;
     aktivtSteg: string;
 }
 
-const DesktopAvtaleSide: React.FunctionComponent<Props> = props => {
-    const [isOpen, setOpen] = useState<boolean>(false);
-    return (
-        <>
-            <div className="avtaleside__desktop">
-                <div className="avtaleside__lenkedeling">
-                    <Lenke onClick={() => setOpen(true)} href="#">
-                        Del lenke til avtalen <ShareIkon />
-                    </Lenke>
-                </div>
-
-                <div className="avtaleside__container">
-                    <Stegmeny
-                        steg={props.avtaleSteg}
-                        aktivtSteg={props.aktivtSteg}
-                    />
-                    <form className="avtaleside__innhold">
-                        {props.avtaleSteg[props.aktivtSteg].komponent}
-                    </form>
-                </div>
-            </div>
-
-            <KopierLenkeModal
-                isOpen={isOpen}
-                lukkModal={() => setOpen(false)}
-            />
-        </>
-    );
-};
+const DesktopAvtaleSide: React.FunctionComponent<Props> = props => (
+    <>
+        <Stegmeny steg={props.avtaleSteg} aktivtSteg={props.aktivtSteg} />
+        <form className="avtaleside__innhold">
+            {props.avtaleSteg[props.aktivtSteg].komponent}
+        </form>
+    </>
+);
 export default DesktopAvtaleSide;
--- src/AvtaleSide/MobilAvtaleSide/MobilAvtaleSide.tsx
+++ src/AvtaleSide/MobilAvtaleSide/MobilAvtaleSide.tsx
@@ -1,16 +1,12 @@
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
-import React, { useState } from 'react';
+import React from 'react';
 import { AvtaleStegType } from '../AvtaleSide';
-import Lenke from 'nav-frontend-lenker';
-import shareIkon from '../../assets/ikoner/share.svg';
-import KopierLenkeModal from '../../komponenter/modal/KopierLenkeModal';
 
 interface Props {
     avtaleSteg: AvtaleStegType;
 }
 
 const MobilAvtaleSide: React.FunctionComponent<Props> = props => {
-    const [isOpen, setOpen] = useState<boolean>(false);
     const ekspanderbartPanel = Object.keys(props.avtaleSteg).map(steg => (
         <div className="avtaleside__ekspanderbart-panel" key={steg}>
             <Ekspanderbartpanel tittel={props.avtaleSteg[steg].label}>
@@ -19,21 +15,7 @@ const MobilAvtaleSide: React.FunctionComponent<Props> = props => {
         </div>
     ));
 
-    return (
-        <>
-            <div className="avtaleside__lenkedeling">
-                <Lenke onClick={() => setOpen(true)} href="#">
-                    Del lenke til avtalen <img src={shareIkon} />
-                </Lenke>
-            </div>
-            <form>{ekspanderbartPanel}</form>
-
-            <KopierLenkeModal
-                isOpen={isOpen}
-                lukkModal={() => setOpen(false)}
-            />
-        </>
-    );
+    return <form>{ekspanderbartPanel}</form>;
 };
 
 export default MobilAvtaleSide;
