2239f7cd30efdff197554135de94fdae0497f38c
Mats Bolstad <mbolstad@gmail.com>
2019-05-13 12:23:41 +0200

    TAG-457: Fjerner enkelte punkter fra veileders instruks. Mer enhetlig design på instrukspanelene.

--- src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
+++ src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import VeilederpanelMedUtklippstavle from '../../../../komponenter/Veilederpanel/VeilederpanelMedUtklippstavleIkon';
 import { Normaltekst, Element } from 'nav-frontend-typografi';
 import BEMHelper from '../../../../utils/bem';
@@ -6,27 +6,31 @@ import './instruks.less';
 
 const cls = BEMHelper('instruks');
 
-const VeilederInstruks = () => {
-    return (
-        <VeilederpanelMedUtklippstavle>
-            <div className={cls.element('subheader')}>
-                <Element>Som veileder må du:</Element>
-            </div>
-            <Normaltekst>1 - Printe ut avtalen</Normaltekst>
-            <Normaltekst>
-                2 - Lage en førsteside i Gosys på bedriftsnummer til
-                arbeidsgiver
-            </Normaltekst>
-            <Normaltekst>3 - Send avtalen til scanning.</Normaltekst>
-            <Normaltekst>
-                4 - Journalfør avtalen på arbeidsgiver bedriftsnummer
-            </Normaltekst>
-            <Normaltekst>5 - Lag oppgave til behandling i Arena</Normaltekst>
-            <Normaltekst>
-                6 - Registrèr tiltaksgjennomføringen i Arena på vanlig måte
-            </Normaltekst>
-        </VeilederpanelMedUtklippstavle>
-    );
-};
+const VeilederInstruks: FunctionComponent = () => (
+    <VeilederpanelMedUtklippstavle>
+        <div className={cls.element('subheader')}>
+            <Element>Som veileder må du</Element>
+        </div>
+        <ul>
+            <li>
+                <Normaltekst>skrive ut avtalen</Normaltekst>
+            </li>
+            <li>
+                <Normaltekst>
+                    lage en førsteside i Gosys på bedriftsnummeret til
+                    arbeidsgiver
+                </Normaltekst>
+            </li>
+            <li>
+                <Normaltekst>sende avtalen til scanning</Normaltekst>
+            </li>
+            <li>
+                <Normaltekst>
+                    registrere tiltaksgjennomføringen i Arena på vanlig måte
+                </Normaltekst>
+            </li>
+        </ul>
+    </VeilederpanelMedUtklippstavle>
+);
 
 export default VeilederInstruks;
