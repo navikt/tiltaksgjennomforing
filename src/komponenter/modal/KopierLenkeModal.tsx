c0b54ebcdca912133221e39e5fa92aae77056a27
Mats Bolstad <mbolstad@gmail.com>
2019-05-13 13:33:32 +0200

    TAG-454: Skjuler lenkedeling på print.

--- src/komponenter/modal/KopierLenkeModal.tsx
+++ src/komponenter/modal/KopierLenkeModal.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import Modal from 'nav-frontend-modal';
-import { Systemtittel, Normaltekst } from 'nav-frontend-typografi';
-import Lenke from 'nav-frontend-lenker';
-import CopyToClipboard from 'react-copy-to-clipboard';
 import { Hovedknapp } from 'nav-frontend-knapper';
+import Lenke from 'nav-frontend-lenker';
+import Modal from 'nav-frontend-modal';
+import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import React from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import { pathTilAvtaleOversikt } from '../../paths';
 import './KopierLenkeModal.less';
 
@@ -12,41 +12,35 @@ interface Props {
     lukkModal: () => void;
 }
 
-const beskrivelseTekst = (
-    <Normaltekst className="kopierlenke__undertittel">
-        For at arbeidsgiver og deltaker skal kunne logge seg inn og fylle ut
-        avtalen må du sende dem lenken vist over. De kan da kunne logge seg inn
-        i avtalen via ID-porten.
-    </Normaltekst>
-);
-
-const KopierLenkeModal: React.FunctionComponent<Props> = (props: Props) => {
-    return (
-        <Modal
-            className="kopierlenke__modal"
-            contentLabel="Del lenke modal"
-            closeButton={true}
-            isOpen={props.isOpen}
-            onRequestClose={() => props.lukkModal()}
-        >
-            <Systemtittel className="kopierlenke__innholdstittel">
-                Kopier lenken til arbeidsgiver og deltaker:
-            </Systemtittel>
-            <div className="kopierlenke__lenkedeling">
-                <div className="kopierlenke__lenke">
-                    <Lenke href={pathTilAvtaleOversikt}>
-                        {pathTilAvtaleOversikt}
-                    </Lenke>
-                </div>
-                <CopyToClipboard text={pathTilAvtaleOversikt}>
-                    <Hovedknapp className="kopierlenke__kopier-knapp">
-                        Kopier lenke
-                    </Hovedknapp>
-                </CopyToClipboard>
+const KopierLenkeModal: React.FunctionComponent<Props> = props => (
+    <Modal
+        className="kopierlenke__modal"
+        contentLabel="Del lenke modal"
+        closeButton={true}
+        isOpen={props.isOpen}
+        onRequestClose={() => props.lukkModal()}
+    >
+        <Systemtittel className="kopierlenke__innholdstittel">
+            Kopier lenken til arbeidsgiver og deltaker:
+        </Systemtittel>
+        <div className="kopierlenke__lenkedeling">
+            <div className="kopierlenke__lenke">
+                <Lenke href={pathTilAvtaleOversikt}>
+                    {pathTilAvtaleOversikt}
+                </Lenke>
             </div>
-            {beskrivelseTekst}
-        </Modal>
-    );
-};
+            <CopyToClipboard text={pathTilAvtaleOversikt}>
+                <Hovedknapp className="kopierlenke__kopier-knapp">
+                    Kopier lenke
+                </Hovedknapp>
+            </CopyToClipboard>
+        </div>
+        <Normaltekst className="kopierlenke__undertittel">
+            For at arbeidsgiver og deltaker skal kunne logge seg inn og fylle ut
+            avtalen må du sende dem lenken vist over. De kan da kunne logge seg
+            inn i avtalen via ID-porten.
+        </Normaltekst>
+    </Modal>
+);
 
 export default KopierLenkeModal;
