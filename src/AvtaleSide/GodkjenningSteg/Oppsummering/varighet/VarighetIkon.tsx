5710397395dc17b290ea4de8666a89d85457933c
Preben Aas <prebenaas@gmail.com>
2019-06-25 13:52:50 +0200

    Endret outline-ikoner på oppsummeringsside til fargede ikoner for å skape mer liv/skille i avtalen

--- src/AvtaleSide/GodkjenningSteg/Oppsummering/varighet/VarighetIkon.tsx
+++ src/AvtaleSide/GodkjenningSteg/Oppsummering/varighet/VarighetIkon.tsx
@@ -4,18 +4,23 @@ const VarighetIkon = () => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
-        height="24"
-        viewBox="0 0 24 24"
+        height="22"
+        viewBox="0 0 24 22"
     >
-        <g
-            fill="none"
-            fillRule="evenodd"
-            stroke="#3E3832"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M4.5 2.5h-4v21h23v-21h-4" />
-            <path d="M4.5.5h3v4h-3zM16.5.5h3v4h-3zM7.5 2.5h9M.5 7.5h23" />
+        <g fill="none" fillRule="evenodd">
+            <path
+                fill="#C42229"
+                d="M17.778 3.23c0-.51.398-.922.889-.922.49 0 .889.413.889.923s-.399.923-.89.923c-.49 0-.888-.413-.888-.923zm-13.334 0c0-.51.398-.922.89-.922.49 0 .888.413.888.923s-.398.923-.889.923c-.49 0-.889-.413-.889-.923zM22.44 0H1.56C.698 0 0 .716 0 1.599V6h24V1.599a1.6 1.6 0 0 0-.953-1.474A1.526 1.526 0 0 0 22.44 0z"
+            />
+            <path
+                fill="#E3E3DA"
+                d="M0 6v14.416C0 21.291.698 22 1.56 22h20.88c.862 0 1.56-.71 1.56-1.584V6H0z"
+            />
+            <path
+                fill="#06893A"
+                fill-rule="nonzero"
+                d="M14.5 15a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0-1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
+            />
         </g>
     </svg>
 );
