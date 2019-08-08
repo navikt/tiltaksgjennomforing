a041c905762ee488814ca71a559282a1abc17019
Malaz Alkoj <malaz.alkoj@nav.no>
2019-08-08 16:02:57 +0200

    info boks til Deltaker,AG,NAv uten i print versjon

--- src/AvtaleSide/AvtaleSide.tsx
+++ src/AvtaleSide/AvtaleSide.tsx
@@ -2,7 +2,7 @@ import AlertStripe from 'nav-frontend-alertstriper';
 import * as React from 'react';
 import { FunctionComponent, ReactNode, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Context, medContext } from '../AvtaleContext';
+import { Context, medContext, Rolle } from '../AvtaleContext';
 import Banner from '../komponenter/Banner/Banner';
 import BEMHelper from '../utils/bem';
 import ArbeidsoppgaverSteg from './ArbeidsoppgaverSteg/ArbeidsoppgaverSteg';
@@ -18,6 +18,10 @@ import MobilAvtaleSide from './MobilAvtaleSide/MobilAvtaleSide';
 import OppfolgingSteg from './OppfolgingSteg/OppfolgingSteg';
 import TilretteleggingSteg from './TilretteleggingSteg/TilretteleggingSteg';
 import TilbakeTilOversiktLenke from './TilbakeTilOversiktLenke/TilbakeTilOversiktLenke';
+import DeltakerInstruks from './GodkjenningSteg/Oppsummering/instruks/DeltakerInstruks';
+import ArbeidsgiverInstruks from './GodkjenningSteg/Oppsummering/instruks/ArbeidsgiverInstruks';
+import VeilederInstruks from './GodkjenningSteg/Oppsummering/instruks/VeilederInstruks';
+import Innholdsboks from '../komponenter/Innholdsboks/Innholdsboks';
 
 interface MatchProps {
     avtaleId: string;
@@ -88,7 +92,16 @@ const AvtaleSide: FunctionComponent<Props> = props => {
     const aktivtSteg = avtaleSteg.find(
         steg => steg.id === props.match.params.stegPath
     );
-
+    const instruks = (rolle: Rolle) => {
+        switch (rolle) {
+            case 'DELTAKER':
+                return <DeltakerInstruks erLaast={props.avtale.erLaast} />;
+            case 'ARBEIDSGIVER':
+                return <ArbeidsgiverInstruks erLaast={props.avtale.erLaast} />;
+            case 'VEILEDER':
+                return <VeilederInstruks />;
+        }
+    };
     return (
         <AvtaleFetcher
             avtaleId={props.match.params.avtaleId}
@@ -106,7 +119,13 @@ const AvtaleSide: FunctionComponent<Props> = props => {
                             >
                                 Avtalen er godkjent av alle parter og låst.
                             </AlertStripe>
-                            <Oppsummering avtale={props.avtale} />
+                            <Oppsummering
+                                avtale={props.avtale}
+                                rolle={props.rolle}
+                            />
+                            <Innholdsboks className={cls.element('infoboks')}>
+                                {instruks(props.rolle)}
+                            </Innholdsboks>
                         </div>
                     );
                 } else if (props.rolle === 'DELTAKER') {
