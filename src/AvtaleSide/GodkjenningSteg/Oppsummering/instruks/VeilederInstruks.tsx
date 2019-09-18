8100058ddd5f224f13fae25c2f05d461d194e1b5
Preben Aas <prebenaas@gmail.com>
2019-09-18 13:36:53 +0200

    Fjernet kommafeil(?)

--- src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
+++ src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
@@ -24,7 +24,7 @@ const VeilederInstruks: FunctionComponent = () => (
         <div className={cls.element('instrukslist')}>
             <Benken className={cls.element('instrukslistsvg')} />
             <Normaltekst>
-                Etter av avtalen er godkjent, ligger oppgaven «Forbered
+                Etter av avtalen er godkjent ligger oppgaven «Forbered
                 tiltaksgjennomføring Arbeidstrening» på kontorets arbeidsbenk.
             </Normaltekst>
         </div>
