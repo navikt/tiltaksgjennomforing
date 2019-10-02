13bb04595db5fdf49675d92f269da9c75f64d585
Michael Johansen <michael.johansen@nav.no>
2019-10-02 14:29:34 +0200

    TAG-860 FRONTEND: Installere Sentry

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,9 @@
 const CracoLessPlugin = require('craco-less');
+const { EnvironmentPlugin } = require('webpack');
 
 module.exports = {
+    webpack: {
+        plugins: [new EnvironmentPlugin(['GIT_COMMIT_HASH'])],
+    },
     plugins: [{ plugin: CracoLessPlugin }],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.less';
+import { init as Sentry, Integrations } from '@sentry/browser';
 
+Sentry({
+    dsn: 'https://1a8f9838972d4082b0d7124f85b74a37@sentry.nav.no/17',
+    release: process.env.GIT_COMMIT_HASH || 'unknown',
+    environment: window.location.hostname,
+    integrations: [new Integrations.Breadcrumbs({ console: false })],
+});
 if (!global.Intl) {
     global.Intl = require('intl');
 }
