94548b0af066939a3f85361fca8838956c01118b
Sindre D Løken <sindre.loken@soprasteria.com>
2019-10-04 14:51:22 +0200

    Fjernet test toggles

--- src/FeatureToggleProvider.tsx
+++ src/FeatureToggleProvider.tsx
@@ -3,11 +3,9 @@ import restService from './services/rest-service';
 
 export enum Feature {
     Lonnstilskudd = 'tag.tiltak.lonnstilskudd',
-    test1 = 'test1',
-    test2 = 'test2',
 }
 
-const alleFeatures = [Feature.Lonnstilskudd, Feature.test1, Feature.test2];
+const alleFeatures = [Feature.Lonnstilskudd];
 
 export interface FeatureToggles {
     [toggles: string]: boolean;
