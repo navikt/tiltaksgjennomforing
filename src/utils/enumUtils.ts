66be5c62c76b41a3f4c72599643b464b69fe7a78
Sindre D Løken <sindre.loken@soprasteria.com>
2019-10-07 09:09:34 +0200

    Fjernet enum funksjon

--- src/utils/enumUtils.ts
+++
@@ -1,7 +0,0 @@
-/** Returner liste av enum-konstanter som ikke er av typen tall */
-const enumValues = (enumType: any) =>
-    Object.keys(enumType).filter(item => {
-        return isNaN(Number(item));
-    });
-
-export default enumValues;
