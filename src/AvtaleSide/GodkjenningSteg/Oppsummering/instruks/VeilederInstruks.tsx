1e4420ba8fb06cbc210d6618676b1715e8466ef1
Preben Ødegård Aas <prebenaas@gmail.com>
2019-10-11 16:23:21 +0200

    Tag-880 feil i veilederinstruks (#182)
    * Endret feil i tekst
    
    * Fjernet svg'er i denne omgang. Ble mye tull med ikke-allignet ikoner

--- src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
+++ src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
@@ -3,34 +3,22 @@ import VeilederpanelMedUtklippstavle from '@/komponenter/Veilederpanel/Veilederp
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import BEMHelper from '@/utils/bem';
 import './instruks.less';
-import { ReactComponent as Benken } from '@/assets/ikoner/benken.svg';
-import { ReactComponent as Info } from '@/assets/ikoner/info.svg';
-import { ReactComponent as Arena } from '@/assets/ikoner/arena.svg';
 
 const cls = BEMHelper('instruks');
 
 const VeilederInstruks: FunctionComponent = () => (
     <VeilederpanelMedUtklippstavle>
         <div className={cls.element('subheader')}>
-            <Element>NB! Ny rutine:</Element>
+            <Element>Ny rutine:</Element>
         </div>
         <div className={cls.element('instrukslist')}>
-            <Arena className={cls.element('instrukslistsvg')} />
             <Normaltekst>
-                Etter at avtalen er godkjent trenger du som veileder kun å{' '}
-                <b> registrere tiltaksgjennomføringen i Arena.</b>
-            </Normaltekst>
-        </div>
-        <div className={cls.element('instrukslist')}>
-            <Benken className={cls.element('instrukslistsvg')} />
-            <Normaltekst>
-                Etter av avtalen er godkjent ligger oppgaven «Forbered
+                Etter at avtalen er godkjent ligger oppgaven «Forbered
                 tiltaksgjennomføring Arbeidstrening» på kontorets arbeidsbenk i
-                Arena.
+                Arena som du må fullføre.
             </Normaltekst>
         </div>
         <div className={cls.element('instrukslist')}>
-            <Info className={cls.element('instrukslistsvg')} />
             <Normaltekst>
                 Avtalen blir automatisk journalført i Gosys, og du trenger
                 derfor ikke å sende inn avtalen til scanning.
