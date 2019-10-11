a351995cc925624e52393477d1a8252b7de196e9
Preben Aas <prebenaas@gmail.com>
2019-10-11 15:58:05 +0200

    Endret feil i tekst

--- src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
+++ src/AvtaleSide/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks.tsx
@@ -5,28 +5,20 @@ import BEMHelper from '@/utils/bem';
 import './instruks.less';
 import { ReactComponent as Benken } from '@/assets/ikoner/benken.svg';
 import { ReactComponent as Info } from '@/assets/ikoner/info.svg';
-import { ReactComponent as Arena } from '@/assets/ikoner/arena.svg';
 
 const cls = BEMHelper('instruks');
 
 const VeilederInstruks: FunctionComponent = () => (
     <VeilederpanelMedUtklippstavle>
         <div className={cls.element('subheader')}>
-            <Element>NB! Ny rutine:</Element>
-        </div>
-        <div className={cls.element('instrukslist')}>
-            <Arena className={cls.element('instrukslistsvg')} />
-            <Normaltekst>
-                Etter at avtalen er godkjent trenger du som veileder kun å{' '}
-                <b> registrere tiltaksgjennomføringen i Arena.</b>
-            </Normaltekst>
+            <Element>Ny rutine:</Element>
         </div>
         <div className={cls.element('instrukslist')}>
             <Benken className={cls.element('instrukslistsvg')} />
             <Normaltekst>
-                Etter av avtalen er godkjent ligger oppgaven «Forbered
+                Etter at avtalen er godkjent ligger oppgaven «Forbered
                 tiltaksgjennomføring Arbeidstrening» på kontorets arbeidsbenk i
-                Arena.
+                Arena som du må fullføre.
             </Normaltekst>
         </div>
         <div className={cls.element('instrukslist')}>
