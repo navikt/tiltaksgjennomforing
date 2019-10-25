a60d1bd960ace2cfa02210aafb6aa2b2a2c1ac8b
Mats Bolstad <mbolstad@gmail.com>
2019-10-25 08:36:26 +0200

    Gjør props optional

--- src/AvtaleOversikt/natur.tsx
+++ src/AvtaleOversikt/natur.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Natur = ({ width, height }: { width: string; height: string }) => {
+const Natur = ({ width, height }: { width?: string; height?: string }) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
