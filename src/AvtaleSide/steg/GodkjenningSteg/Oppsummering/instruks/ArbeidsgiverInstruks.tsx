cb5e0ca07e4c801ab06acd5f249cecf8b8c88ebe
Preben Aas <prebenaas@gmail.com>
2019-11-18 17:06:38 +0100

    Lagt til ny informasjon om krav til arbeidsgiver

--- src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/instruks/ArbeidsgiverInstruks.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/instruks/ArbeidsgiverInstruks.tsx
@@ -4,11 +4,13 @@ import { FunctionComponent } from 'react';
 import VeilederpanelMedUtklippstavle from '@/komponenter/Veilederpanel/VeilederpanelMedUtklippstavleIkon';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import EkstbanderbartPanelRad from '@/komponenter/EkspanderbartPanelRad/EkstbanderbartPanelRad';
-import { ReactComponent as RettighetsHammerIkon } from '@/assets/ikoner/law.svg';
-import { ReactComponent as DrofteMedAnsattePersonOpplysning } from '@/assets/ikoner/drofteMedAnsattePersonOpplysning.svg';
-import { ReactComponent as SkadeErstatning } from '@/assets/ikoner/skadeErstatning.svg';
-import { ReactComponent as BehandlingPersonOpplysning } from '@/assets/ikoner/behandlingPersonOpplysning.svg';
+import { ReactComponent as LovIkon } from '@/assets/ikoner/lov.svg';
+import { ReactComponent as AnsatteIkon } from '@/assets/ikoner/ansatte.svg';
+import { ReactComponent as SkadeIkon } from '@/assets/ikoner/skade.svg';
+import { ReactComponent as PersonOpplysningIkon } from '@/assets/ikoner/personopplysninger.svg';
+import { ReactComponent as SykIkon } from '@/assets/ikoner/sykdom.svg';
 import BEMHelper from '@/utils/bem';
+import EksternLenke from '@/komponenter/navigation/EksternLenke';
 
 const cls = BEMHelper('instruks');
 interface Props {
@@ -29,6 +31,9 @@ const ArbeidsgiverInstruks: FunctionComponent<Props> = props => (
                     <Normaltekst>ha forsikring for deltaker på arbeidstrening</Normaltekst>
                 </li>
                 <li>
+                    <Normaltekst>følge folketrygdlovens regler for egenmelding og sykmelding</Normaltekst>
+                </li>
+                <li>
                     <Normaltekst>sørge for at tiltaksplassen ikke erstatter en vanlig stilling i bedriften</Normaltekst>
                 </li>
                 <li>
@@ -41,42 +46,80 @@ const ArbeidsgiverInstruks: FunctionComponent<Props> = props => (
         </VeilederpanelMedUtklippstavle>
 
         <Ekspanderbartpanel border={true} tittel="Les mer om kravene">
-            <EkstbanderbartPanelRad svgIkon={<RettighetsHammerIkon />} headerTekst={{ tekst: 'Arbeidsmiljøloven' }}>
-                En arbeidssøker som deltar på arbeidstrening blir i de fleste situasjoner regnet som en ordinær
-                arbeidstaker etter arbeidsmiljøloven. Det betyr at arbeidsgivers og deltaker plikter til arbeidsmiljø og
-                krav til kontrolltiltak også gjelder når arbeidssøkeren er på arbeidstrening. Det inkluderer blant annet
-                arbeidstid, vern mot diskriminering og straff ved overtredelse av lovens bestemmelser.
+            <EkstbanderbartPanelRad svgIkon={<LovIkon />} headerTekst={{ tekst: 'Arbeidsmiljøloven' }}>
+                <p>
+                    Selv om deltakeren ikke er ansatt, skal dere følge arbeidsmiljølovens bestemmelser om arbeidsgivers
+                    og arbeidstakers plikter, krav til arbeidsmiljø og krav til kontrolltiltak. Det inkluderer også
+                    arbeidstid, vern mot diskriminering og straff ved overtredelse av lovens bestemmelser.
+                </p>
+                <p>
+                    <EksternLenke href="https://lovdata.no/dokument/NL/lov/2005-06-17-62">
+                        Les mer om arbeidsmiljøloven her
+                    </EksternLenke>
+                </p>
             </EkstbanderbartPanelRad>
+
             <EkstbanderbartPanelRad
-                svgIkon={<SkadeErstatning />}
+                svgIkon={<SkadeIkon />}
                 headerTekst={{
                     tekst: 'Yrkesskadeforsikring og skadeerstatning',
                 }}
             >
-                Den som er i arbeidstrening er omfattet av arbeidsgiverens yrkesskadeforsikring. Arbeidsgiveren er
-                ansvarlig for skade som personen i arbeidstrening gjør, enten forsettlig eller uaktsomt. Du kan søke om
-                inkluderingstilskudd for å dekke eventuelle kostnader i forbindelse med forsikringsansvaret.
+                <p>
+                    Dere er ansvarlig dersom deltakeren skader seg på jobb. Du kan søke om inkluderingstilskudd for å
+                    dekke utgiftene dere har til forsikringen.{' '}
+                    <EksternLenke href="https://lovdata.no/dokument/NL/lov/1989-06-16-65">
+                        Les mer om yrkesskadeforsikring her
+                    </EksternLenke>
+                </p>
+                <p>
+                    Dere er etter{' '}
+                    <EksternLenke href="https://lovdata.no/dokument/NL/lov/1969-06-13-26">
+                        lov om skadeserstatning
+                    </EksternLenke>{' '}
+                    ansvarlig for skade som deltakeren gjør forsettlig eller uaktsomt.{' '}
+                </p>
             </EkstbanderbartPanelRad>
+
             <EkstbanderbartPanelRad
-                svgIkon={<DrofteMedAnsattePersonOpplysning />}
+                svgIkon={<SykIkon />}
+                headerTekst={{
+                    tekst: 'Folketrygdlovens regler for egenmelding og sykmelding',
+                }}
+            >
+                Folketrygdlovens regler følges når det gjelder bruk av egenmelding og sykmelding for egen og barns
+                sykdom.
+            </EkstbanderbartPanelRad>
+
+            <EkstbanderbartPanelRad
+                svgIkon={<AnsatteIkon />}
                 headerTekst={{
                     tekst: 'Drøft med representanter for de ansatte',
                 }}
             >
-                Du må drøfte inngåelse av avtale om arbeidstrening med representanter for de ansatte i din bedrift før
-                en eventuell avtaleinngåelse. Tillitsvalgte og verneombud skal ivareta de ansattes interesser, og en
-                tiltaksplass skal ikke erstatte en vanlig stilling i bedriften.
+                <p>
+                    Før dere gjør en avtale om å ta imot deltakeren på arbeidstrening, må dere drøfte dette med
+                    representanter for ansatte i bedriften.
+                </p>
+                <p>Tiltaksplasser skal ikke erstatte en vanlig stilling i bedriften.</p>
             </EkstbanderbartPanelRad>
 
             <EkstbanderbartPanelRad
-                svgIkon={<BehandlingPersonOpplysning />}
+                svgIkon={<PersonOpplysningIkon />}
                 headerTekst={{ tekst: 'Behandling av personopplysninger' }}
             >
-                Som arbeidsgiver må du sikre at all behandling av personopplysninger utføres etter krav til personvern,
-                taushetsplikt og informasjonssikkerhet. Taushetsbelagte og sensitive personopplysninger skal ikke sendes
-                mellom deg og NAV på usikrede og ukrypterte kommunikasjonskanaler, slik som usikret epost og faks.
-                Senest innen 12 uker etter at tiltaksgjennomføringen er avsluttet, skal du eller databehandleren slette
-                personopplysninger om den du har i arbeidstrening.
+                <p>
+                    Dere har ansvar for å følge{' '}
+                    <EksternLenke href="https://lovdata.no/dokument/NL/lov/2018-06-15-38">
+                        personopplysningsloven
+                    </EksternLenke>
+                </p>
+
+                <p>
+                    Dere kan ikke sende taushetsbelagte og sensitive personopplysninger til NAV på usikret epost. Dere
+                    må slette alle personopplysningene om deltakeren senest innen 12 uker etter at tiltaket er
+                    avsluttet.
+                </p>
             </EkstbanderbartPanelRad>
         </Ekspanderbartpanel>
     </>
