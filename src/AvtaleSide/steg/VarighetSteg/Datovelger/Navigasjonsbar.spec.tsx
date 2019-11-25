4c0b5749dab04c484ed9182de875abb8140d3651
Malaz Alkoj <malaz.alkoj@nav.no>
2019-11-25 10:10:31 +0100

    merge med master

--- src/AvtaleSide/steg/VarighetSteg/Datovelger/Navigasjonsbar.spec.tsx
+++ src/AvtaleSide/steg/VarighetSteg/Datovelger/Navigasjonsbar.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Navigasjonsbar from './Navigasjonsbar';
+import Navigasjonsbar from './navigasjonsbar';
 
 test('Test that <Navigasjonsbar> renders correctly', () => {
     const wrapper = shallow(<Navigasjonsbar showPreviousButton={true} showNextButton={true} />);
