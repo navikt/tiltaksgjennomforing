3c01273e76ef185d42a5df153c635d746ad1b9d5
Mats Bolstad <mbolstad@gmail.com>
2019-11-27 22:38:51 +0100

    Reduserer kodeduplisering

--- src/AvtaleSide/steg/GodkjenningSteg/GodkjenningSteg.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/GodkjenningSteg.tsx
@@ -6,28 +6,46 @@ import DeltakerInstruks from '@/AvtaleSide/steg/GodkjenningSteg/Oppsummering/ins
 import ArbeidsgiverInstruks from '@/AvtaleSide/steg/GodkjenningSteg/Oppsummering/instruks/ArbeidsgiverInstruks';
 import VeilederInstruks from '@/AvtaleSide/steg/GodkjenningSteg/Oppsummering/instruks/VeilederInstruks';
 import Innholdsboks from '@/komponenter/Innholdsboks/Innholdsboks';
+import { Systemtittel } from 'nav-frontend-typografi';
+import Avtaleparter from '@/AvtaleSide/steg/GodkjenningSteg/Oppsummering/Avtaleparter/Avtaleparter';
+import SkrivUtKnapp from '@/komponenter/SkrivUtKnapp/SkrivUtKnapp';
 
 type Props = {
     oppsummering: JSX.Element;
 };
 
-const GodkjenningSteg: React.FunctionComponent<Props & Context> = props => {
-    return (
-        <>
-            <AvtaleStatus avtale={props.avtale} rolle={props.rolle} />
+const GodkjenningSteg: React.FunctionComponent<Props & Context> = props => (
+    <>
+        <AvtaleStatus avtale={props.avtale} rolle={props.rolle} />
+        <Innholdsboks>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                }}
+            >
+                <Systemtittel style={{ marginBottom: '2rem' }}>
+                    {props.avtale.erLaast ? 'Oppsummering av inngått avtale' : 'Godkjenning av avtale'}
+                </Systemtittel>
+
+                {props.avtale.erLaast && <SkrivUtKnapp />}
+            </div>
+
+            <Avtaleparter {...props.avtale} />
             {props.oppsummering}
-            <Innholdsboks className={'avtaleside__infoboks'}>
-                {props.rolle === 'DELTAKER' && <DeltakerInstruks erLaast={props.avtale.erLaast} />}
-                {props.rolle === 'ARBEIDSGIVER' && <ArbeidsgiverInstruks erLaast={props.avtale.erLaast} />}
-                {props.rolle === 'VEILEDER' && <VeilederInstruks />}
-            </Innholdsboks>
-            <Godkjenning
-                avtale={props.avtale}
-                rolle={props.rolle}
-                endreGodkjenning={props.godkjenn}
-                godkjennPaVegne={props.godkjennPaVegne}
-            />
-        </>
-    );
-};
+        </Innholdsboks>
+        <Innholdsboks className={'avtaleside__infoboks'}>
+            {props.rolle === 'DELTAKER' && <DeltakerInstruks erLaast={props.avtale.erLaast} />}
+            {props.rolle === 'ARBEIDSGIVER' && <ArbeidsgiverInstruks erLaast={props.avtale.erLaast} />}
+            {props.rolle === 'VEILEDER' && <VeilederInstruks />}
+        </Innholdsboks>
+        <Godkjenning
+            avtale={props.avtale}
+            rolle={props.rolle}
+            endreGodkjenning={props.godkjenn}
+            godkjennPaVegne={props.godkjennPaVegne}
+        />
+    </>
+);
 export default medContext(GodkjenningSteg);
--- src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/OppsummeringArbeidstrening/OppsummeringArbeidstrening.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/OppsummeringArbeidstrening/OppsummeringArbeidstrening.tsx
@@ -1,55 +1,26 @@
-import { ReactComponent as PrinterSvg } from '@/assets/ikoner/printer.svg';
 import { medContext, Rolle } from '@/AvtaleContext';
-import Innholdsboks from '@/komponenter/Innholdsboks/Innholdsboks';
 import { Avtale } from '@/types/avtale';
-import { Knapp } from 'nav-frontend-knapper';
-import { Systemtittel } from 'nav-frontend-typografi';
 import * as React from 'react';
 import { FunctionComponent } from 'react';
-import Avtaleparter from '../Avtaleparter/Avtaleparter';
 import MaalOppsummering from '../maalOppsummering/MaalOppsummering';
 import OppfolgingOppsummering from '../oppfølging/OppfolgingOppsummering';
 import OppgaverOppsummering from '../oppgaveOppsummering/OppgaverOppsummering';
 import Tilrettelegging from '../tilrettelegging/Tilrettelegging';
 import VarighetOppsummering from '../varighet/VarighetOppsummering';
-import './OppsummeringArbeidstrening.less';
-import BEMHelper from '@/utils/bem';
 
 interface Props {
     avtale: Avtale;
     rolle: Rolle;
 }
 
-const printAvtale = () => {
-    window.print();
-};
-
-const cls = BEMHelper('oppsummering-arbeidstrening');
-
 const OppsummeringArbeidstrening: FunctionComponent<Props> = props => (
-    <Innholdsboks>
-        <div className={cls.element('header')}>
-            <Systemtittel className={cls.element('tittel')}>
-                {props.avtale.erLaast ? 'Oppsummering av inngått avtale' : 'Godkjenning av avtale'}
-            </Systemtittel>
-
-            {props.avtale.erLaast && (
-                <Knapp className={cls.element('print-knapp')} onClick={printAvtale}>
-                    skriv ut avtale
-                    <div className={cls.element('print-knapp__ikon')}>
-                        <PrinterSvg />
-                    </div>
-                </Knapp>
-            )}
-        </div>
-
-        <Avtaleparter {...props.avtale} />
+    <>
         <MaalOppsummering {...props.avtale} />
         <OppgaverOppsummering {...props.avtale} />
         <VarighetOppsummering {...props.avtale} />
         <OppfolgingOppsummering {...props.avtale} />
         <Tilrettelegging {...props.avtale} />
-    </Innholdsboks>
+    </>
 );
 
 export default medContext(OppsummeringArbeidstrening);
