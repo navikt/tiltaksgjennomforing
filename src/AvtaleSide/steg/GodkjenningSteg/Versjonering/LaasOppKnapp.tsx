ebe5fb8eb83a999677947c431ce18fb4002394ba
Mats Bolstad <mbolstad@gmail.com>
2019-12-04 09:34:23 +0100

    Endrer navn på knapp og fjerner CSS

--- src/AvtaleSide/steg/GodkjenningSteg/Versjonering/LaasOppKnapp.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Versjonering/LaasOppKnapp.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
-import BEMHelper from '@/utils/bem';
 import { Avtale } from '@/types/avtale';
 import Innholdsboks from '@/komponenter/Innholdsboks/Innholdsboks';
-import './LaasOppKnapp.less';
 import LagreKnapp from '@/komponenter/LagreKnapp/LagreKnapp';
 import { Rolle } from '@/AvtaleContext';
 
-const cls = BEMHelper('laasOppKnapp');
 interface Props {
     rolle: Rolle;
     avtale: Avtale;
@@ -17,7 +14,7 @@ const LaasOppKnapp: React.FunctionComponent<Props> = props => {
     const låsOppAvtaleklikk = () => {
         if (
             window.confirm(
-                'Er du sikker på at du vil låse opp avtalen og opprette en ny versjon?\nDu og arbeidsgiver kan endre innhold i avtalen og alle må godjhenne på nytt.'
+                'Er du sikker på at du vil låse opp avtalen og opprette en ny versjon?\nDu og arbeidsgiver kan endre innhold i avtalen og alle må godkjenne på nytt.'
             )
         ) {
             return props.laasOpp();
@@ -29,13 +26,7 @@ const LaasOppKnapp: React.FunctionComponent<Props> = props => {
         <>
             {props.rolle === 'VEILEDER' && props.avtale.kanLåsesOpp && (
                 <Innholdsboks>
-                    {' '}
-                    <LagreKnapp
-                        className={cls.element('laasoppknapp')}
-                        label={'Lag ny versjon'}
-                        lagre={låsOppAvtaleklikk}
-                    >
-                        {' '}
+                    <LagreKnapp label={'Lås opp avtale'} lagre={låsOppAvtaleklikk}>
                         Lås opp avtalen
                     </LagreKnapp>
                 </Innholdsboks>
