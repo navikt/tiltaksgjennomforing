f93cd2c1f966c98792f13a2b0aff495702ddd3b3
Preben Aas <prebenaas@gmail.com>
2019-12-15 16:24:08 +0100

    Slettet en mappe ved en feil. Cherry-picket meg tilbake og la til endringene påny

--- src/AvtaleSide/AvbryteAvtalen/AvbryteAvtalen.tsx
+++ src/AvtaleSide/AvbryteAvtalen/AvbryteAvtalen.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import './AvbryteAvtalen.less';
+import { ReactComponent as AvbruttIkon } from '@/assets/ikoner/Avbryte-avtale.svg';
 import { Context } from '@/AvtaleContext';
 import Lenke from 'nav-frontend-lenker';
-import { ReactComponent as AvbruttIkon } from '@/assets/ikoner/file-remove.svg';
+import * as React from 'react';
+import './AvbryteAvtalen.less';
 
 interface Props {
     avbrytOnclick: () => void;
@@ -11,11 +11,7 @@ interface Props {
 
 const AvbryteAvtalen = (props: Props) => (
     <div className="avbryteavtalen">
-        <Lenke
-            onClick={props.avbrytOnclick}
-            href="#"
-            className="avbryteavtalen__lenke"
-        >
+        <Lenke onClick={props.avbrytOnclick} href="#" className="avbryteavtalen__lenke">
             <AvbruttIkon className="avbryteavtalen__ikon" />
             Avbryt avtale
         </Lenke>
--- src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/instruks/ArbeidsgiverInstruks.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/instruks/ArbeidsgiverInstruks.tsx
@@ -4,7 +4,7 @@ import { FunctionComponent } from 'react';
 import VeilederpanelMedUtklippstavle from '@/komponenter/Veilederpanel/VeilederpanelMedUtklippstavleIkon';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import EkstbanderbartPanelRad from '@/komponenter/EkspanderbartPanelRad/EkstbanderbartPanelRad';
-import { ReactComponent as RettighetsHammerIkon } from '@/assets/ikoner/law.svg';
+import { ReactComponent as RettighetsHammerIkon } from '@/assets/ikoner/lov.svg';
 import { ReactComponent as DrofteMedAnsattePersonOpplysning } from '@/assets/ikoner/drofteMedAnsattePersonOpplysning.svg';
 import { ReactComponent as SkadeErstatning } from '@/assets/ikoner/skadeErstatning.svg';
 import { ReactComponent as BehandlingPersonOpplysning } from '@/assets/ikoner/behandlingPersonOpplysning.svg';
