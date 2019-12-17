3ccf8d5f5a29676db91b7fbafd803999303944c8
Sindre Dahl Løken <sindre.loken@nav.no>
2019-12-17 16:58:21 +0100

    Laget type fra array i maalkategorier og laget array fra type i featureToggleProvider

--- src/types/maalkategorier.ts
+++ src/types/maalkategorier.ts
@@ -1,16 +1,10 @@
-export type Maalkategori =
-    | 'FÅ_JOBB_I_BEDRIFTEN'
-    | 'ARBEIDSERFARING'
-    | 'UTPRØVING'
-    | 'SPRÅKOPPLÆRING'
-    | 'OPPNÅ_FAGBREV_KOMPETANSEBEVIS'
-    | 'ANNET';
-
-export const maalkategorier: Maalkategori[] = [
+export const maalkategorier = [
     'FÅ_JOBB_I_BEDRIFTEN',
     'ARBEIDSERFARING',
     'UTPRØVING',
     'SPRÅKOPPLÆRING',
     'OPPNÅ_FAGBREV_KOMPETANSEBEVIS',
     'ANNET',
-];
+] as const;
+
+export type Maalkategori = typeof maalkategorier[number];
