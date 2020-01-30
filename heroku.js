efeb8fd2d68c94ca369282f5f7216830d8cdd0c2
Mats Bolstad <mbolstad@gmail.com>
2020-01-30 13:46:01 +0100

    Eget nodescript for heroku

---
+++ heroku.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const server = express();
+
+server.use('/tiltaksgjennomforing', express.static(path.join(__dirname, 'build')));
+
+server.get('/tiltaksgjennomforing/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log('Server listening on port', port);
+});
