d9fc09bb5f2a501bb62b6f0e1b2ef52fa22e07c4
Preben <prebenaas@gmail.com>
2020-02-04 15:25:26 +0100

    Fjernet ubrukt prop etter review

--- src/AvtaleSide/AvbryteAvtalen/AvbryteAvtalen.tsx
+++ src/AvtaleSide/AvbryteAvtalen/AvbryteAvtalen.tsx
@@ -1,12 +1,10 @@
 import { ReactComponent as AvbruttIkon } from '@/assets/ikoner/Avbryte-avtale.svg';
-import { Context } from '@/AvtaleContext';
 import Lenke from 'nav-frontend-lenker';
 import * as React from 'react';
 import './AvbryteAvtalen.less';
 
 interface Props {
     avbrytOnclick: () => void;
-    lagreAvtale?: Context;
 }
 
 const AvbryteAvtalen = (props: Props) => (
