d4be8653c82cabf48f7961a6b660b257fc407835
havardmj <Havard.Myrvold.Johannessen@nav.no>
2020-02-07 08:45:55 +0100

    fikset camelcase pa funksjonsnavn i filen og fjernet ekstra mellomrom i yaml fil

--- plugins/decoratorhtmlwebpackplugin.js
+++ plugins/decoratorhtmlwebpackplugin.js
@@ -13,26 +13,26 @@ const htmlinsert = [
     { inject: 'megamenuResources', from: 'megamenu-resources' },
 ];
 
-const decoratorhtmlwebpackplugin = (enablemenu = false, title = '') => {
+const decoratorHtmlWebpackPlugin = (enablemenu = false, title = '') => {
     return {
         overrideWebpackConfig: ({ webpackConfig }) => {
-            const plugin = gethtmlwebpackplugin(webpackConfig.plugins);
+            const plugin = getHtmlWebpackPlugin(webpackConfig.plugins);
             if (plugin) {
                 plugin.options.title = title;
-                enablemenu ? getmenu(plugin) : addelements(plugin, false);
+                enablemenu ? getMenu(plugin) : addElements(plugin, false);
             }
             return webpackConfig;
         },
     };
 };
 
-const addelements = (plugin, documentisfetched, document = {}) => {
+const addElements = (plugin, documentisfetched, document = {}) => {
     htmlinsert.forEach(element => {
-        plugin.options[element.inject] = documentisfetched ? getelement(document, element.from) : '';
+        plugin.options[element.inject] = documentisfetched ? getElement(document, element.from) : '';
     });
 };
 
-const gethtmlwebpackplugin = plugins => {
+const getHtmlWebpackPlugin = plugins => {
     return plugins.find(plugin => {
         if (plugin.constructor.name === 'HtmlWebpackPlugin') {
             return plugin;
@@ -40,25 +40,25 @@ const gethtmlwebpackplugin = plugins => {
     });
 };
 
-const enablebackup = plugin => {
+const enableBackup = plugin => {
     process.env.ENABLE_EXTERNAL_MENU = 'enable';
-    addelements(plugin, false);
+    addElements(plugin, false);
 };
 
-const getelement = (document, id) => {
+const getElement = (document, id) => {
     const prop = 'innerHTML';
     return document.getElementById(id)[prop];
 };
 
-const getmenu = plugin => {
+const getMenu = plugin => {
     request({ method: 'GET', uri: url }, (error, response, body) => {
         if (!error && response.statusCode >= 200 && response.statusCode < 400) {
             const { document } = new JSDOM(body).window;
-            addelements(plugin, true, document);
+            addElements(plugin, true, document);
         } else {
-            enablebackup(plugin);
+            enableBackup(plugin);
         }
     });
 };
 
-module.exports = decoratorhtmlwebpackplugin;
+module.exports = decoratorHtmlWebpackPlugin;
