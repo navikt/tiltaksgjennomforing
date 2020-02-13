d7f11a78094e2f35620d7b9135b6a1b3710280d3
Sindre Dahl Løken <sindre.loken@nav.no>
2020-02-13 14:19:33 +0100

    Tilpass tester

--- src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/OppsummeringLonnstilskudd/OppsummeringLonnstilskudd.spec.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/OppsummeringLonnstilskudd/OppsummeringLonnstilskudd.spec.tsx
@@ -1,22 +1,26 @@
+import lonnstilskuddAvtaleMock from '@/mocking/lonnstilskudd-avtale-mock';
 import { shallow } from 'enzyme';
 import React from 'react';
 import BeregningTilskuddOppsummering from '../BeregningTilskuddOppsummering/BeregningTilskuddOppsummering';
-import LonnstilskuddOppsummering from '../LonnstilskuddOppsummering/LonnstilskuddOppsummering';
 import OppfolgingOppsummering from '../oppfølging/OppfolgingOppsummering';
 import StillingsOppsummering from '../StillingsOppsummering/StillingsOppsummering';
+import Tilrettelegging from '../tilrettelegging/Tilrettelegging';
+import VarighetOppsummering from '../varighet/VarighetOppsummering';
 import OppsummeringLonnstilskudd from './OppsummeringLonnstilskudd';
-import lonnstilskuddAvtaleMock from '@/mocking/lonnstilskudd-avtale-mock';
 
 const wrapper = shallow(<OppsummeringLonnstilskudd avtaleinnhold={lonnstilskuddAvtaleMock} />);
 
 test('Skal rendre <StillingsOppsummering>', () => {
     expect(wrapper.find(StillingsOppsummering)).toHaveLength(1);
 });
-test('Skal rendre <LonnstilskuddOppsummering>', () => {
-    expect(wrapper.find(LonnstilskuddOppsummering)).toHaveLength(1);
+test('Skal rendre <VarighetOppsummering>', () => {
+    expect(wrapper.find(VarighetOppsummering)).toHaveLength(1);
 });
 test('Skal rendre <OppfolgingOppsummering>', () => {
     expect(wrapper.find(OppfolgingOppsummering)).toHaveLength(1);
+});
+test('Skal rendre <Tilrettelegging>', () => {
+    expect(wrapper.find(Tilrettelegging)).toHaveLength(1);
 });
 test('Skal rendre <BeregningTilskuddOppsummering>', () => {
     expect(wrapper.find(BeregningTilskuddOppsummering)).toHaveLength(1);
