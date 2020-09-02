c4d22081ab3c5b547c5205c350e6d7afa7928b08
Mats Bolstad <mbolstad@gmail.com>
2020-09-02 12:06:25 +0200

    Nytt filter for status

---
+++ src/AvtaleOversikt/Filtrering/StatusFilter.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { FunctionComponent, useState } from 'react';
+import { OptionProps } from '@/komponenter/form/SelectInput';
+import { FiltreringProps } from '@/AvtaleOversikt/Filtrering/Filtrering';
+import { Filter } from '@/AvtaleOversikt/Filtrering/Filter';
+import { Radio } from 'nav-frontend-skjema';
+
+const StatusFilter: FunctionComponent<FiltreringProps> = props => {
+    const [valgtStatus, setValgtStatus] = useState('');
+
+    const statuser: OptionProps[] = [
+        { value: '', label: 'Alle' },
+        { value: 'PÅBEGYNT', label: 'Påbegynt' },
+        { value: 'MANGLER_GODKJENNING', label: 'Mangler godkjenning' },
+        { value: 'KLAR_FOR_OPPSTART', label: 'Klar for oppstart' },
+        { value: 'GJENNOMFØRES', label: 'Gjennomføres' },
+        { value: 'AVSLUTTET', label: 'Avsluttet' },
+        { value: 'AVBRUTT', label: 'Avbrutt' },
+    ];
+
+    return (
+        <Filter tittel="Status">
+            {statuser.map(s => (
+                <Radio
+                    key={s.label}
+                    label={s.label}
+                    name={'status'}
+                    value={s.value}
+                    checked={s.value === valgtStatus}
+                    onChange={event => {
+                        const nyStatus = event.currentTarget.value;
+                        setValgtStatus(nyStatus);
+                        props.endreSøk({ status: nyStatus });
+                    }}
+                />
+            ))}
+        </Filter>
+    );
+};
+
+export default StatusFilter;
