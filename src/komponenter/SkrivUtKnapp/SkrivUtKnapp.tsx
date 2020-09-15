dc7d1b02a0ea3d3962adbb589d4f151513f3428a
Sindre Dahl Løken <sindre.loken@nav.no>
2020-09-15 16:12:44 +0200

    svgo pre-comiit og fix printer ikon

--- src/komponenter/SkrivUtKnapp/SkrivUtKnapp.tsx
+++ src/komponenter/SkrivUtKnapp/SkrivUtKnapp.tsx
@@ -1,8 +1,8 @@
 import { ReactComponent as PrinterSvg } from '@/assets/ikoner/printer.svg';
+import BEMHelper from '@/utils/bem';
 import { Knapp } from 'nav-frontend-knapper';
 import * as React from 'react';
 import { FunctionComponent } from 'react';
-import BEMHelper from '@/utils/bem';
 import './SkrivUtKnapp.less';
 
 const printAvtale = () => {
@@ -15,7 +15,7 @@ const SkrivUtKnapp: FunctionComponent = () => (
     <Knapp className={cls.className} onClick={printAvtale}>
         Skriv ut avtale
         <div className={cls.element('ikon')}>
-            <PrinterSvg />
+            <PrinterSvg id="printerSvg" style={{ width: '24', height: '24' }} />
         </div>
     </Knapp>
 );
