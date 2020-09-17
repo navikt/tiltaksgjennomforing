4018e21329979f75132711a7cd8fbc99cd69ddd2
Sindre Dahl Løken <sindre.loken@nav.no>
2020-09-17 09:47:57 +0200

    catcher og viser feilmedlinger pa restkall i bekreftelsemodal

--- src/types/feilkode.ts
+++ src/types/feilkode.ts
@@ -3,6 +3,6 @@ export type Feilkode = 'SAMTIDIGE_ENDRINGER' | 'ALT_MA_VAERE_FYLT_UT' | 'IKKE_VA
 export const Feilmeldinger: { [key in Feilkode]: string } = {
     ALT_MA_VAERE_FYLT_UT: 'Alt må være fylt ut før du kan godkjenne',
     SAMTIDIGE_ENDRINGER:
-        'Du må oppdatere siden før du kan lagre eller godkjenne. Det er gjort endringer i avtalen som du ikke har sett.',
+        'Du må oppdatere siden før du kan lagre, godkjenne eller gjøre andre endringer. Det er gjort endringer i avtalen som du ikke har sett.',
     IKKE_VALGT_PART: 'Ikke valgt part',
 };
