19a8b47278f83b7150fdc3e9c052f82f48ad8a6b
Mats Bolstad <mbolstad@gmail.com>
2020-09-18 11:27:13 +0200

    Feilkodemappinger for varighet

--- src/types/feilkode.ts
+++ src/types/feilkode.ts
@@ -13,7 +13,10 @@ export type Feilkode =
     | 'KAN_IKKE_LAASES_OPP'
     | 'ER_ALLEREDE_VEILEDER'
     | 'START_ETTER_SLUTT'
-    | 'IKKE_AKSEPTERT_UTKAST';
+    | 'IKKE_AKSEPTERT_UTKAST'
+    | 'VARIGHET_FOR_LANG_MIDLERTIDIG_LONNSTILSKUDD'
+    | 'VARIGHET_FOR_LANG_MENTOR'
+    | 'VARIGHET_FOR_LANG_ARBEIDSTRENING';
 
 export const Feilmeldinger: { [key in Feilkode]: string } = {
     ALT_MA_VAERE_FYLT_UT: 'Alt må være fylt ut før du kan godkjenne',
@@ -32,4 +35,7 @@ export const Feilmeldinger: { [key in Feilkode]: string } = {
     ER_ALLEREDE_VEILEDER: 'Innlogget bruker er allerede veileder på denne avtalen',
     START_ETTER_SLUTT: 'Startdato er etter sluttdato',
     IKKE_AKSEPTERT_UTKAST: 'Kan ikke godkjenne før veileder har akseptert avtaleutkast',
+    VARIGHET_FOR_LANG_MIDLERTIDIG_LONNSTILSKUDD: 'Avtalens varighet er maksimalt 24 måneder',
+    VARIGHET_FOR_LANG_MENTOR: 'Avtalens varighet er maksimalt 36 måneder',
+    VARIGHET_FOR_LANG_ARBEIDSTRENING: 'Avtalens varighet er maksimalt 18 måneder',
 };
