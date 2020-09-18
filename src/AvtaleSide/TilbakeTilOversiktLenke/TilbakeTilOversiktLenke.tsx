3d43b56a1d737efb8679f7967cc9ece884fbaf3f
Sindre Dahl Løken <sindre.loken@nav.no>
2020-09-18 10:44:35 +0200

    Trukket lagring av endringer ut fra TilbakeTilOversiktLenke til en prop. Denne brukes ogsa utenfor AvtaleContext. (#454)
    Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>
    Co-authored-by: Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>

--- src/AvtaleSide/TilbakeTilOversiktLenke/TilbakeTilOversiktLenke.tsx
+++ src/AvtaleSide/TilbakeTilOversiktLenke/TilbakeTilOversiktLenke.tsx
@@ -1,29 +1,16 @@
-import VenstreChevron from 'nav-frontend-chevron/lib/venstre-chevron';
-import React, { FunctionComponent, useContext } from 'react';
-import { Link } from 'react-router-dom';
 import { pathTilOversikt } from '@/paths';
-import { ApiError } from '@/types/errors';
-import { AvtaleContext } from '@/AvtaleContext';
+import VenstreChevron from 'nav-frontend-chevron/lib/venstre-chevron';
+import React, { FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
 import './TilbakeTilOversiktLenke.less';
 
-const TilbakeTilOversiktLenke: FunctionComponent<{}> = () => {
-    const context = useContext(AvtaleContext);
+type Props = {
+    onClick?: () => void;
+};
 
-    const tilbakeTilOversikt = async () => {
-        if (context.harUlagredeEndringer()) {
-            try {
-                await context.lagreAvtale();
-            } catch (error) {
-                if (error instanceof ApiError) {
-                    return context.visFeilmelding(error.message);
-                }
-                throw error;
-            }
-        }
-    };
-
+const TilbakeTilOversiktLenke: FunctionComponent<Props> = props => {
     return (
-        <Link to={pathTilOversikt} className="lenke" onClick={tilbakeTilOversikt}>
+        <Link to={pathTilOversikt} className="lenke" onClick={props.onClick}>
             <VenstreChevron className="tilbaketiloversikt__chevron" />
             Tilbake til oversikt
         </Link>
