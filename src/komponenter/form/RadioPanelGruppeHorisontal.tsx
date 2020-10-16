bc677ab6f5fff2040e88f4fae9265c0865652540
Sindre Dahl Løken <sindre.loken@nav.no>
2020-10-16 15:32:35 +0200

    revertet radiopanelgruppehoirsontal

--- src/komponenter/form/RadioPanelGruppeHorisontal.tsx
+++ src/komponenter/form/RadioPanelGruppeHorisontal.tsx
@@ -1,13 +1,12 @@
 import { RadioPanel, RadioPanelGruppeProps, RadioProps } from 'nav-frontend-skjema/lib/radio-panel-gruppe';
 import * as React from 'react';
 
-const RadioPanelGruppeHorisontal = (props: RadioPanelGruppeProps & { disabled?: boolean }) => {
-    const { radios, name, checked, onChange, disabled } = props;
+const RadioPanelGruppeHorisontal = (props: RadioPanelGruppeProps) => {
+    const { radios, name, checked, onChange } = props;
     return (
         <div className="opprett-avtale__tiltakstypeWrapper">
             {radios.map((radio: RadioProps) => (
                 <RadioPanel
-                    disabled={disabled}
                     name={name}
                     key={`${name}-${radio.value}`}
                     checked={checked === radio.value}
