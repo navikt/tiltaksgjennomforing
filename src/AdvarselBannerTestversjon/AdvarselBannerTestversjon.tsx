88aa8f69e544d2d01893fc387bb864378684f978
Sindre Dahl Løken <sindre.loken@nav.no>
2020-11-11 15:45:29 +0100

    instruks om hvordan teste opprettelse av avtale pa labs

--- src/AdvarselBannerTestversjon/AdvarselBannerTestversjon.tsx
+++ src/AdvarselBannerTestversjon/AdvarselBannerTestversjon.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
-import './AdvarselBannerTestversjon.less';
+import LesMerPanel from '@/komponenter/LesMerPanel/LesMerPanel';
 import BEMHelper from '@/utils/bem';
+import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import React from 'react';
+import './AdvarselBannerTestversjon.less';
 
 const cls = BEMHelper('advarsel-banner-testversjon');
 
@@ -19,6 +20,13 @@ const AdvarselBannerTestversjon = () => {
                     <br />
                     Hvis du er arbeidsgiver eller deltaker logger du deg på her:
                     arbeidsgiver.nav.no/tiltaksgjennomforing.
+                    <LesMerPanel åpneLabel="Slik tester du registrering av avtaler" lukkLabel="Lukk">
+                        For å teste hele flyten, kan du logge inn som arbeidsgiver, opprette en avtale på fnr:
+                        <b>15000000067</b> og bedriftnr: <b>999999999</b>.
+                        <br />
+                        Vi ber også om at du ikke registrer ekte data i denne løsningen, da dette er en åpen
+                        testversjon.
+                    </LesMerPanel>
                 </AlertStripeAdvarsel>
             )}
         </>
