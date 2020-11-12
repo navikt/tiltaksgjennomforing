9465e4615ed63f9173df0fe993bf3cecb554df23
Sindre Dahl Løken <sindre.loken@nav.no>
2020-11-12 11:59:49 +0100

    wip

--- src/AvtaleOversikt/IngenAvtaler/arbeidsgiver/IngenAvtalerArbeidsgiver.tsx
+++ src/AvtaleOversikt/IngenAvtaler/arbeidsgiver/IngenAvtalerArbeidsgiver.tsx
@@ -1,11 +1,10 @@
+import DuManglerRettigheterIAltinn from '@/AvtaleOversikt/IngenAvtaler/arbeidsgiver/DuManglerRettigheterIAltinn';
 import { InnloggetBrukerContext } from '@/InnloggingBoundary/InnloggingBoundary';
+import { tiltakstypeTekst } from '@/messages';
 import { TiltaksType } from '@/types/avtale';
 import React, { FunctionComponent, useContext } from 'react';
-import './IngenAvtalerArbeidsgiver.less';
-import DuManglerRettigheterIAltinn from '@/AvtaleOversikt/IngenAvtaler/arbeidsgiver/DuManglerRettigheterIAltinn';
-
-import { tiltakstypeTekst } from '@/messages';
 import BoksMedTekstOgTilgangstabell from './BoksMedTekstOgTilgangstabell';
+import './IngenAvtalerArbeidsgiver.less';
 
 type Props = {
     bedriftNr?: string;
@@ -13,19 +12,21 @@ type Props = {
 };
 
 const IngenAvtalerArbeidsgiver: FunctionComponent<Props> = props => {
-    const { tilganger } = useContext(InnloggetBrukerContext);
+    const { tilganger, altinnOrganisasjoner } = useContext(InnloggetBrukerContext);
 
     if (!props.bedriftNr) {
         return <DuManglerRettigheterIAltinn />;
     }
 
     const fellesProps = { bedriftNr: props.bedriftNr, tilganger };
+    const valgtBedrift = altinnOrganisasjoner.find(o => o.OrganizationNumber === props.bedriftNr);
+    console.log(valgtBedrift);
 
     if (!tilganger[props.bedriftNr] || tilganger[props.bedriftNr].length === 0) {
         return (
             <BoksMedTekstOgTilgangstabell
                 {...fellesProps}
-                overskrift={'Du mangler tilgang til alle tiltakstypene'}
+                overskrift={`Du mangler tilgang til alle tiltakstypene i ${valgtBedrift?.Name} (${valgtBedrift?.OrganizationNumber})`}
                 visTekst={false}
             />
         );
@@ -35,7 +36,11 @@ const IngenAvtalerArbeidsgiver: FunctionComponent<Props> = props => {
         if (tilganger[props.bedriftNr].includes(props.tiltakstype)) {
             // Har tilgang til valgt tiltakstype
             return (
-                <BoksMedTekstOgTilgangstabell {...fellesProps} overskrift={'Finner ingen avtaler'} visTekst={true} />
+                <BoksMedTekstOgTilgangstabell
+                    {...fellesProps}
+                    overskrift={`Finner ingen avtaler i ${valgtBedrift?.Name} (${valgtBedrift?.OrganizationNumber})`}
+                    visTekst={true}
+                />
             );
         } else {
             return (
@@ -49,7 +54,13 @@ const IngenAvtalerArbeidsgiver: FunctionComponent<Props> = props => {
     }
 
     // Har ikke valgt tiltakstype. Har en eller alle tilganger
-    return <BoksMedTekstOgTilgangstabell {...fellesProps} overskrift={'Finner ingen avtaler'} visTekst={true} />;
+    return (
+        <BoksMedTekstOgTilgangstabell
+            {...fellesProps}
+            overskrift={`Finner ingen avtaler i ${valgtBedrift?.Name} (${valgtBedrift?.OrganizationNumber})`}
+            visTekst={true}
+        />
+    );
 };
 
 export default IngenAvtalerArbeidsgiver;
