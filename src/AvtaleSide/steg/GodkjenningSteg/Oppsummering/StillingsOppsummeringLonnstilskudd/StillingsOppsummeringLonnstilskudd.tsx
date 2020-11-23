a5421d71a704272f905d7250336902624e399a1a
Sindre Dahl Løken <sindre.loken@nav.no>
2020-11-23 10:30:22 +0100

    justering av spacing og ikon godkjenningsside

--- src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/StillingsOppsummeringLonnstilskudd/StillingsOppsummeringLonnstilskudd.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Oppsummering/StillingsOppsummeringLonnstilskudd/StillingsOppsummeringLonnstilskudd.tsx
@@ -1,19 +1,20 @@
+import { ReactComponent as StillingIkon } from '@/assets/ikoner/toolbox.svg';
 import VerticalSpacer from '@/komponenter/layout/VerticalSpacer';
+import { stillingstype } from '@/messages';
 import { Stilling } from '@/types/avtale';
 import { Element } from 'nav-frontend-typografi';
 import React, { FunctionComponent } from 'react';
 import SjekkOmVerdiEksisterer from '../SjekkOmVerdiEksisterer/SjekkOmVerdiEksisterer';
 import Stegoppsummering from '../Stegoppsummering/Stegoppsummering';
-import { stillingstype } from '@/messages';
 
 const StillingsOppsummering: FunctionComponent<Stilling> = props => (
-    <Stegoppsummering tittel="Stilling">
+    <Stegoppsummering tittel="Stilling" ikon={<StillingIkon />}>
         <Element>Stillingstittel</Element>
         <SjekkOmVerdiEksisterer verdi={props.stillingstittel} />
-        <VerticalSpacer sixteenPx={true} />
+        <VerticalSpacer rem={2} />
         <Element>Arbeidsoppgaver</Element>
         <SjekkOmVerdiEksisterer verdi={props.arbeidsoppgaver} />
-        <VerticalSpacer sixteenPx={true} />
+        <VerticalSpacer rem={2} />
         <Element>Stillingstype</Element>
         <SjekkOmVerdiEksisterer verdi={props.stillingstype} formatertVerdi={stillingstype[props.stillingstype!]} />
     </Stegoppsummering>
