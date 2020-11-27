05541a89c5c6c15cd099072e526f5570cb6c6969
Sindre Dahl Løken <sindre.loken@nav.no>
2020-11-27 11:17:14 +0100

    legger til react/sentry og bytter til egen sentry errorboundary for bedre handtering av feil Co-authored-by: Mats Bolstad <mats.bolstad@nav.no> Co-authored-by: Håvard Myrvold Johannessen <havard.myrvold.johannessen@nav.no>

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
+import { init as Sentry, Integrations } from '@sentry/react';
 import 'babel-polyfill';
-import 'react-app-polyfill/ie9';
 import * as React from 'react';
+import 'react-app-polyfill/ie9';
 import * as ReactDOM from 'react-dom';
+import 'url-search-params-polyfill';
 import App from './App';
 import './index.less';
-import { init as Sentry, Integrations } from '@sentry/browser';
-import 'url-search-params-polyfill';
 
 Sentry({
     dsn: 'https://3a5b579938bc4d6c9011c48d34af18f8@sentry.gc.nav.no/4',
--- src/komponenter/ErrorBoundary.tsx
+++ src/komponenter/ErrorBoundary.tsx
@@ -1,34 +1,31 @@
-import amplitude from '@/utils/amplitude';
+import * as Sentry from '@sentry/react';
 import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
-import { Normaltekst } from 'nav-frontend-typografi';
-import React from 'react';
+import { Normaltekst, Undertekst, Undertittel } from 'nav-frontend-typografi';
+import React, { FunctionComponent } from 'react';
+import VerticalSpacer from './layout/VerticalSpacer';
 
-// Denne er kopiert fra: https://reactjs.org/docs/error-boundaries.html
-class ErrorBoundary extends React.Component {
-    state = { hasError: false, error: null };
-    static getDerivedStateFromError(error: any) {
-        return {
-            hasError: true,
-            error: error,
-        };
-    }
-
-    getErrorString = (error: Error | null) => `${error && error.toString()}`;
-
-    render() {
-        if (this.state.hasError) {
-            amplitude.logEvent('#tiltak-errorboundary', { feil: this.getErrorString(this.state.error) });
-            return (
-                <AlertStripeAdvarsel>
-                    <Normaltekst>Det har skjedd en uventet feil. Forsøk å laste siden på nytt.</Normaltekst>
-                    <Normaltekst>
-                        Teknisk feilkode: <i>{this.getErrorString(this.state.error)}</i>
-                    </Normaltekst>
-                </AlertStripeAdvarsel>
-            );
-        }
-        return this.props.children;
-    }
-}
+const ErrorBoundary: FunctionComponent = props => {
+    return (
+        <Sentry.ErrorBoundary
+            fallback={({ error, componentStack, resetError }) => (
+                <>
+                    <AlertStripeAdvarsel>
+                        <Undertittel>Det har oppstått en uventet feil. Forsøk å laste siden på nytt.</Undertittel>
+                        <VerticalSpacer rem={1} />
+                        <Normaltekst>
+                            Teknisk feilkode: <i>{error.toString()}</i>
+                        </Normaltekst>
+                        <VerticalSpacer rem={0.5} />
+                        <Undertekst>
+                            <i style={{ color: 'gray' }}>{componentStack}</i>
+                        </Undertekst>
+                    </AlertStripeAdvarsel>
+                </>
+            )}
+        >
+            {props.children}
+        </Sentry.ErrorBoundary>
+    );
+};
 
 export default ErrorBoundary;
