f3a47786d320265ef094d4209215f94bb7317a42
Sindre Dahl Løken <sindre.loken@nav.no>
2020-12-07 15:24:23 +0100

    oppdatert urlPrefix

--- sentry.js
+++ sentry.js
@@ -1,5 +1,6 @@
 //https://medium.com/@vshab/create-react-app-and-sentry-cde1f15cbaa
 const SentryCli = require('@sentry/cli');
+
 async function createReleaseAndUpload() {
     const release = process.env.GIT_COMMIT_HASH;
     if (!release) {
@@ -14,7 +15,7 @@ async function createReleaseAndUpload() {
         console.log('Uploading source maps');
         await cli.releases.uploadSourceMaps(release, {
             include: ['build/static/js'],
-            urlPrefix: '~/static/js',
+            urlPrefix: '~/tiltaksgjennomforing/static/js',
             rewrite: false,
         });
         console.log('Finalizing release');
