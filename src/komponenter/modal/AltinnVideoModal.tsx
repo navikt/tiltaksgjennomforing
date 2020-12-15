8f08b9d747cda71aad8e1079132edb681d82bede
Sindre Dahl Løken <sindre.loken@nav.no>
2020-12-15 14:53:04 +0100

    instruks om hva som vises i video som leses av skjermleser

--- src/komponenter/modal/AltinnVideoModal.tsx
+++ src/komponenter/modal/AltinnVideoModal.tsx
@@ -14,6 +14,13 @@ interface Props {
 
 const cls = BEMHelper('videomodal');
 
+const videoInstruks = `I videoen vises det hvordan man delegere en rettighet i Altinn.
+Dette gjøres på følgende måte:
+1. Logge seg inn på altinn.no.
+2. Trykke på innstillinger for bedriften du skal delegere rettigheter i.
+3. Velg andre med rettigheter til virksomheten og personen du skal gi rettighet til.
+4. Søk etter rettigheten, huk av for alle rettigheter og klikk gi rettigheter.`;
+
 const AltinnVideoModal: React.FunctionComponent<Props> = props => {
     return (
         <Modal
@@ -27,13 +34,13 @@ const AltinnVideoModal: React.FunctionComponent<Props> = props => {
                 <Systemtittel>Slik tildeler du roller og rettigheter i Altinn.</Systemtittel>
             </div>
             <MediaQuery minWidth={576}>
-                <video width="700" height="450" controls={true} onTimeUpdate={trackVideo}>
+                <video width="700" height="450" controls={true} onTimeUpdate={trackVideo} aria-label={videoInstruks}>
                     <source src={video} type="video/mp4" />
                     Nettleseren din støtter ikke denne videoen.
                 </video>
             </MediaQuery>
             <MediaQuery maxWidth={575}>
-                <video width="300" height="300" controls={true} onTimeUpdate={trackVideo}>
+                <video width="300" height="300" controls={true} onTimeUpdate={trackVideo} aria-label={videoInstruks}>
                     <source src={video} type="video/mp4" />
                     Nettleseren din støtter ikke denne videoen.
                 </video>
