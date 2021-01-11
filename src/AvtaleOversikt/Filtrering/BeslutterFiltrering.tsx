ad33ef821a1f6706305c9f5445fe2a58e801a64d
Millad Dagdoni <millad.dagdoni@nav.no>
2021-01-11 13:46:23 +0100

    introdusert bedriftfilter for BeslutterFiltrering.tsx
    Co-authored-by: Bjarte Tynning <bjarte.tynning@nav.no>

--- src/AvtaleOversikt/Filtrering/BeslutterFiltrering.tsx
+++ src/AvtaleOversikt/Filtrering/BeslutterFiltrering.tsx
@@ -3,11 +3,13 @@ import React, { FunctionComponent } from 'react';
 import { FiltreringProps } from './VeilederFiltrering';
 import TilskuddPeriodeStatusFilter from '@/AvtaleOversikt/Filtrering/TilskuddPeriodeStatusFilter';
 import BEMHelper from '@/utils/bem';
+import { BedriftFilter } from '@/AvtaleOversikt/Filtrering/BedriftFilter';
 
 const cls = BEMHelper('filtrering');
 const BeslutterFiltrering: FunctionComponent<FiltreringProps> = props => {
     return (
         <div className={cls.className}>
+            <BedriftFilter {...props} />
             <TilskuddPeriodeStatusFilter {...props} />
             <TiltakstypeFilter {...props} erBeslutter />
         </div>
