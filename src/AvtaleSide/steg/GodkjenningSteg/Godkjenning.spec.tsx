4985bc1786541538a63d23a6dd5164d34a99c071
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-01-25 19:29:01 +0100

    fjernet ubrukt imports

--- src/AvtaleSide/steg/GodkjenningSteg/Godkjenning.spec.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Godkjenning.spec.tsx
@@ -1,13 +1,10 @@
 import arbeidstreningAvtaleMock from '@/mocking/arbeidstrening-avtale-mock';
-import { Avtale } from '@/types/avtale';
 import { shallow } from 'enzyme';
 import React from 'react';
 import Godkjenning from './Godkjenning';
-import { rejects } from 'assert';
 
 test('Test that <Godkjenning> renders correctly', () => {
     const avtale = arbeidstreningAvtaleMock;
-
     const wrapper = shallow(<Godkjenning avtale={avtale} />);
     expect(wrapper).toHaveLength(1);
 });
