8899ac97eaf2474f4652b878cd1fab9a7a80af0a
Sindre Dahl Løken <sindre.loken@nav.no>
2021-03-19 13:13:51 +0100

    tekstfiks testbanner Co-authored-by: Mats Bolstad <mats.bolstad@nav.no>

--- src/AdvarselBannerTestversjon/AdvarselBannerTestversjon.tsx
+++ src/AdvarselBannerTestversjon/AdvarselBannerTestversjon.tsx
@@ -21,8 +21,8 @@ const AdvarselBannerTestversjon = () => {
                     Hvis du er arbeidsgiver eller deltaker logger du deg på her:
                     arbeidsgiver.nav.no/tiltaksgjennomforing.
                     <LesMerPanel åpneLabel="Slik tester du registrering av avtaler" lukkLabel="Lukk">
-                        For å teste hele flyten, kan du logge inn som veileder, opprette en avtale på fnr:
-                        <b>23090170716</b> og bedriftnr: <b>999999999</b>.
+                        For å teste hele flyten, kan du logge inn som veileder, opprette en avtale på fnr:{' '}
+                        <b>23090170716</b> (fiktivt) og bedriftnr: <b>999999999</b>.
                         <br />
                         Vi ber også om at du ikke registrer ekte data i denne løsningen, da dette er en åpen
                         testversjon.
