32f5b039696fa02a393eb70f70c582ee0a1e5982
MagnusRom <magnus.skalleberg.rom@nav.no>
2021-06-03 10:36:37 +0200

    svgo Test

--- svgo.sh
+++ svgo.sh
@@ -2,5 +2,5 @@
 for i in $(git diff  --cached --diff-filter=AMR --name-only|grep .svg)
 do
   echo "Minifiserer og gir unik id til svg: $i"
-  svgo --enable=prefixIds --disable=removeViewBox --disable=removeNonInheritableGroupAttrs $i
+  svgo --disable=removeNonInheritableGroupAttrs --enable=prefixIds --disable=removeViewBox  $i
 done
