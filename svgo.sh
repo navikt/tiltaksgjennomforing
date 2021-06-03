f92026a40cc5c257613b59a6ae843432ac4ee5a8
MagnusRom <magnus.skalleberg.rom@nav.no>
2021-06-03 13:12:25 +0200

    removed removeNonInheritableGroupAttrs

--- svgo.sh
+++ svgo.sh
@@ -2,5 +2,5 @@
 for i in $(git diff  --cached --diff-filter=AMR --name-only|grep .svg)
 do
   echo "Minifiserer og gir unik id til svg: $i"
-  svgo --disable=removeNonInheritableGroupAttrs --enable=prefixIds --disable=removeViewBox  --disable=removeUnknownsAndDefaults   $i
+  svgo --enable=prefixIds --disable=removeViewBox  --disable=removeUnknownsAndDefaults   $i
 done
