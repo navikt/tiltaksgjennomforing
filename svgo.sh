48a21155f39356db1d34de07037af29bf55d2f40
MagnusRom <magnus.skalleberg.rom@nav.no>
2021-06-03 13:16:11 +0200

    removed UnknownsAndDefaults

--- svgo.sh
+++ svgo.sh
@@ -2,5 +2,5 @@
 for i in $(git diff  --cached --diff-filter=AMR --name-only|grep .svg)
 do
   echo "Minifiserer og gir unik id til svg: $i"
-  svgo  --enable=prefixIds --disable=removeViewBox  --disable=removeUnknownsAndDefaults   $i
+  svgo  --enable=prefixIds --disable=removeViewBox $i
 done
