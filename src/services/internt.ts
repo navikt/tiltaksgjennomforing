0a59c8b5a4816fd35d08784ab70aa0778d41757a
Sindre Dahl Løken <sindre.loken@nav.no>
2021-08-31 13:02:17 +0200

    mer axios

--- src/services/internt.ts
+++ src/services/internt.ts
@@ -2,18 +2,17 @@ import { Innloggingskilde } from '@/types/innlogget-bruker';
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/tiltaksgjennomforing',
     withCredentials: true,
     headers: { Pragma: 'no-cache', 'Cache-Control': 'no-cache', 'Content-Type': 'application/json' },
 });
 
 
 export const hentInnloggingskilder = async (): Promise<Innloggingskilde[]> => {
-    const response = await api.get<Innloggingskilde[]>(`/innloggingskilder`);
+    const response = await api.get<Innloggingskilde[]>(`/tiltaksgjennomforing/innloggingskilder`);
     return response.data;
 };
 
 export const sjekkOmMenySkalBrukes = async (url: string): Promise<boolean> => {
-    const response = await fetch(url);
-    return response.json();
+    const response = await api.get<boolean>(url);
+    return response.data;
 };
