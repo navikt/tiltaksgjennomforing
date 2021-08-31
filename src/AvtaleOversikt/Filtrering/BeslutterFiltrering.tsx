50a271f6f1e0cbd95027cfc2760f72f078534304
Mats Bolstad <mbolstad@gmail.com>
2021-08-31 13:53:47 +0200

    Sortering for beslutter

--- src/AvtaleOversikt/Filtrering/BeslutterFiltrering.tsx
+++ src/AvtaleOversikt/Filtrering/BeslutterFiltrering.tsx
@@ -4,11 +4,13 @@ import { FiltreringProps } from './VeilederFiltrering';
 import TilskuddPeriodeStatusFilter from '@/AvtaleOversikt/Filtrering/TilskuddPeriodeStatusFilter';
 import BEMHelper from '@/utils/bem';
 import { BedriftFilter } from '@/AvtaleOversikt/Filtrering/BedriftFilter';
+import Sortering from "@/AvtaleOversikt/Sortering";
 
 const cls = BEMHelper('filtrering');
 const BeslutterFiltrering: FunctionComponent<FiltreringProps> = props => {
     return (
         <div className={cls.className}>
+            <Sortering endreSøk={props.endreSøk} />
             <BedriftFilter {...props} />
             <TilskuddPeriodeStatusFilter {...props} />
             <TiltakstypeFilter {...props} erBeslutter={true} />
