cd16f4ad61e1d79c1ed0595b79a5c7a80531ad96
Sindre Dahl Løken <sindre.loken@nav.no>
2021-08-31 12:30:57 +0200

    bruker axios i kall for å hente innloggingskilder fra frontend-backend

--- src/services/internt.ts
+++ src/services/internt.ts
@@ -1,8 +1,16 @@
 import { Innloggingskilde } from '@/types/innlogget-bruker';
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: '/tiltaksgjennomforing',
+    withCredentials: true,
+    headers: { Pragma: 'no-cache', 'Cache-Control': 'no-cache', 'Content-Type': 'application/json' },
+});
+
 
 export const hentInnloggingskilder = async (): Promise<Innloggingskilde[]> => {
-    const response = await fetch('/tiltaksgjennomforing/innloggingskilder');
-    return response.json();
+    const response = await api.get<Innloggingskilde[]>(`/innloggingskilder`);
+    return response.data;
 };
 
 export const sjekkOmMenySkalBrukes = async (url: string): Promise<boolean> => {
