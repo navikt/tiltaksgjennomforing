c7d26306520c1b9ffee1ee616d464a90e3f6d341
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-10-28 16:32:42 +0200

    lagt til genererFnrDatoStringFraFnr i fnrUtils

--- src/utils/fnrUtils.ts
+++ src/utils/fnrUtils.ts
@@ -3,6 +3,17 @@
  * @param verdi
  * @returns {boolean}
  */
+
+interface GyldigAarhundre {
+    aarhundre: number;
+    gyldig: boolean;
+}
+
+export interface VellykketGenerertIsoDatoString {
+    isoDatostring: string;
+    vellykketgenerering: boolean;
+}
+
 const validerFnr = (verdi: string): boolean => {
     const fodselsnr = verdi.toString();
     if (!fodselsnr || fodselsnr.length !== 11) {
@@ -29,4 +40,52 @@ const validerFnr = (verdi: string): boolean => {
     return k1 === parseInt(fodselsnr.charAt(9), 10) && k2 === parseInt(fodselsnr.charAt(10), 10);
 };
 
-export { validerFnr };
+const isDNumber = (n: number) => n > 40 && n < 72;
+
+const hentAarhundreTilFnr = (kSiffer: number, fodselAar: number): GyldigAarhundre => {
+    switch (true) {
+        case kSiffer >= 0 && kSiffer <= 499:
+            return { aarhundre: 19, gyldig: true };
+        case kSiffer >= 500 && kSiffer <= 749 && fodselAar >= 55:
+            return { aarhundre: 18, gyldig: true };
+        case kSiffer >= 500 && kSiffer <= 999 && fodselAar <= 39:
+            return { aarhundre: 20, gyldig: true };
+        case kSiffer >= 900 && kSiffer <= 999 && fodselAar >= 40:
+            return { aarhundre: 19, gyldig: true };
+        default:
+            return { aarhundre: 0, gyldig: false };
+    }
+};
+
+const lagDatoString = (isoDatoVerdier: number[]): string => {
+    return isoDatoVerdier
+        .map((v) => {
+            const datoFragment = v.toFixed(0);
+            return datoFragment.length === 1 ? '0'.concat(datoFragment) : datoFragment;
+        })
+        .join('-');
+};
+
+const genererFnrDatoStringFraFnr = (n: string): VellykketGenerertIsoDatoString => {
+    try {
+        const dag = parseInt(n.substring(0, 2), 10);
+        const mnd = parseInt(n.substring(2, 4), 10);
+        const aar = parseInt(n.substring(4, 6), 10);
+        const kontrollsiffer = parseInt(n.substring(6, 9), 10);
+        const gyldigAarhundre = hentAarhundreTilFnr(kontrollsiffer, aar);
+
+        if (gyldigAarhundre.gyldig) {
+            const isoDatoStringFraFnr = lagDatoString([
+                gyldigAarhundre.aarhundre * 100 + aar,
+                mnd,
+                isDNumber(dag) ? dag - 40 : dag,
+            ]);
+            return { isoDatostring: isoDatoStringFraFnr, vellykketgenerering: true };
+        }
+    } catch (err) {
+        console.warn('feilet med generering av iso-datostring fra fnr ', err);
+    }
+    return { isoDatostring: '', vellykketgenerering: false };
+};
+
+export { validerFnr, genererFnrDatoStringFraFnr };
