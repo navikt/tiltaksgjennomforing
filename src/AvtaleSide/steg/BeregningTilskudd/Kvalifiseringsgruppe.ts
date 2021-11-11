2092219a8c53781b9863b7d2474f6116a090d623
havardmj <Havard.Myrvold.Johannessen@nav.no>
2021-11-11 11:50:26 +0100

    Lagt til stotte funksjoner i Kvalifiseringsgruppe, for sjekk av gyldighet, og generering av feiltekst.

--- src/AvtaleSide/steg/BeregningTilskudd/Kvalifiseringsgruppe.ts
+++ src/AvtaleSide/steg/BeregningTilskudd/Kvalifiseringsgruppe.ts
@@ -10,3 +10,41 @@ export enum Kvalifiseringsgruppe {
     SYKMELDT_OPPFOLGING_PA_ARBEIDSPLASSEN = 'VURDI', // Sykmeldt, oppfølging på arbeidsplassen
     SYKMELDT_UTEN_ARBEIDSGIVER = 'VURDU', // Sykmeldt uten arbeidsgiver
 }
+
+const erGyldingKvalifiseringForMidlertidiglonnstilskudd = (kode: string): boolean => {
+    return (
+        kode === Kvalifiseringsgruppe.SPESIELT_TILPASSET_INNSATS ||
+        kode === Kvalifiseringsgruppe.SITUASJONSBESTEMT_INNSATS ||
+        kode === Kvalifiseringsgruppe.VARIG_TILPASSET_INNSATS
+    );
+};
+
+const erGyldingKvalifiseringForVariglonntilskudd = (kode: string): boolean =>
+    kode === Kvalifiseringsgruppe.VARIG_TILPASSET_INNSATS;
+
+const hentFeilMeldingForUgyldingKvalifiseringsgruppe = (kode: string): string => {
+    return 'Kandidat er registrert med status '
+        .concat(hentKvalifiseringsgruppeTekst(kode))
+        .concat('. Denne gruppen kvalifiserer ikke til Arbeidstiltaket.');
+};
+
+const hentKvalifiseringsgruppeTekst = (kode: string): string => {
+    switch (kode) {
+        case Kvalifiseringsgruppe.BEHOV_FOR_ARBEIDSEVNEVURDERING:
+            return 'behov for arbeidsevnevurdering';
+        case Kvalifiseringsgruppe.STANDARD_INNSATS:
+            return 'standard innsats';
+        case Kvalifiseringsgruppe.IKKE_VURDERT:
+            return 'ikke vurdert';
+        case Kvalifiseringsgruppe.RETTIGHETER_ETTER_FTRL_KAP11:
+            return 'rettigheter etter Ftrl, kapittel 11';
+        case Kvalifiseringsgruppe.HELSERELATERT_ARBEIDSRETTET_OPPFOLGING_I_NAV:
+            return 'helserelatert arbeidsrettet oppfølging';
+        case Kvalifiseringsgruppe.SYKMELDT_OPPFOLGING_PA_ARBEIDSPLASSEN:
+            return 'sykmeldt, oppfølging på arbeidsplassen';
+        case Kvalifiseringsgruppe.SYKMELDT_UTEN_ARBEIDSGIVER:
+            return 'sykmeldt uten arbeidsgiver';
+        default:
+            return 'Ikke funnet';
+    }
+};
