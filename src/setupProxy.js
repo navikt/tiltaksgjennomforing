d151a4cbd9af3990f4671db1edeff41557cbcc5a
Sindre Dahl Løken <sindre.loken@nav.no>
2022-01-11 16:47:03 +0100

    lagt til en import

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const fetch = require('node-fetch');
 const whitelist = require('./whitelist');
 const apiProxy = require('../server/api-proxy');
+const tokenx = require('../server/tokenx');
 
 const brukLokalLogin = process.env.NODE_ENV === 'development';
 
