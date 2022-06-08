7f795ddcde1203c02cb038da356c587b2b0acc89
havardmj <havard.myrvold.johannessen@nav.no>
2022-06-08 18:16:13 +0200

    formatert endreKostnadssted

--- src/BeslutterSide/beslutterPanel/TilskuddsperiodeEndreKostnadssted.tsx
+++ src/BeslutterSide/beslutterPanel/TilskuddsperiodeEndreKostnadssted.tsx
@@ -1,39 +1,33 @@
-import React, {FunctionComponent, useContext } from "react";
-import PakrevdInput from "@/komponenter/PakrevdInput/PakrevdInput";
-import VerticalSpacer from "@/komponenter/layout/VerticalSpacer";
+import React, { FunctionComponent, useContext } from 'react';
+import PakrevdInput from '@/komponenter/PakrevdInput/PakrevdInput';
+import VerticalSpacer from '@/komponenter/layout/VerticalSpacer';
 
-import {AvtaleContext} from "@/AvtaleProvider";
-import BEMHelper from "@/utils/bem";
-import {TilskuddsperiodeContext} from "@/BeslutterSide/BeslutterSide";
-
+import { AvtaleContext } from '@/AvtaleProvider';
+import BEMHelper from '@/utils/bem';
+import { TilskuddsperiodeContext } from '@/BeslutterSide/BeslutterSide';
 
 const TilskuddsperiodeEndreKostnadssted: FunctionComponent = () => {
     const { avtale } = useContext(AvtaleContext);
-    const { enhet, setEnhet, enhetFeil } = useContext(TilskuddsperiodeContext)
+    const { enhet, setEnhet, enhetFeil } = useContext(TilskuddsperiodeContext);
     const { gjeldendeTilskuddsperiode } = avtale;
+    const cls = BEMHelper('beslutter-panel');
 
-
-
-    const cls = BEMHelper('beslutter-panel')
-
-    if(gjeldendeTilskuddsperiode && gjeldendeTilskuddsperiode.status !== 'UBEHANDLET') return null;
-
+    if (gjeldendeTilskuddsperiode && gjeldendeTilskuddsperiode.status !== 'UBEHANDLET') return null;
 
     return (
-            <>
-                <div className={cls.element('input-wrapper')}>
-                    <PakrevdInput
-                        bredde="S"
-                        label=""
-                        verdi={enhet}
-                        settVerdi={(verdi) => setEnhet(verdi)}
-                        maxLength={4}
-                        feil={enhetFeil}
-                    />
-                    <VerticalSpacer rem={1} />
-                </div>
-
-            </>
-    )
-}
+        <>
+            <div className={cls.element('input-wrapper')}>
+                <PakrevdInput
+                    bredde="S"
+                    label=""
+                    verdi={enhet}
+                    settVerdi={(verdi) => setEnhet(verdi)}
+                    maxLength={4}
+                    feil={enhetFeil}
+                />
+                <VerticalSpacer rem={1} />
+            </div>
+        </>
+    );
+};
 export default TilskuddsperiodeEndreKostnadssted;
