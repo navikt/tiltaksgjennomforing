a60fae6d416e8ab95bb07e9fc1784ec67c57f474
Sindre Dahl Løken <sindre.loken@nav.no>
2022-06-24 13:17:39 +0200

    fikser en jest-test for en komponent som har blitt avhengig av avtaleContext.
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/AvtaleSide/steg/KontaktInformasjonSteg/KontaktinfoSteg.spec.tsx
+++ src/AvtaleSide/steg/KontaktInformasjonSteg/KontaktinfoSteg.spec.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import { AvtaleContext, Context } from '@/AvtaleProvider';
+import arbeidstreningAvtaleMock from '@/mocking/arbeidstrening-avtale-mock';
 import { shallow } from 'enzyme';
+import React from 'react';
 import KontaktinfoSteg from './KontaktinfoSteg';
 
 test('Test that <KontaktinfoSteg> renders correctly', () => {
-    const wrapper = shallow(<KontaktinfoSteg />);
+    const wrapper = shallow(
+        <AvtaleContext.Provider value={{ avtale: arbeidstreningAvtaleMock } as Context}>
+            <KontaktinfoSteg />
+        </AvtaleContext.Provider>
+    );
+    
     expect(wrapper).toHaveLength(1);
 });
