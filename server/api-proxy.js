72a3c8615d1c0639bc5fa0ab71266b69c9ce2548
millad <milladd@gmail.com>
2022-07-12 10:19:52 +0200

    ignorer 401 om det er kall til feature endepunkt

--- server/api-proxy.js
+++ server/api-proxy.js
@@ -3,7 +3,7 @@ const tokenx = require('./tokenx')
 
 const setup = (app, tokenxClient) => {
     app.use('/tiltaksgjennomforing/api', (req, res, next) => {
-        if (!req.headers['authorization']) {
+        if (!req.originalUrl.includes("feature") && !req.headers['authorization']) {
             res.status(401).send();
         } else {
             next();
@@ -17,9 +17,11 @@ const setup = (app, tokenxClient) => {
                 return req.originalUrl.replace("/tiltaksgjennomforing/api", "/tiltaksgjennomforing-api");
             },
             proxyReqOptDecorator: async (options, req) => {
+              if(!req.originalUrl.includes("feature")){
                 const accessToken = await tokenx.getTokenExchangeAccessToken(tokenxClient, req);
                 options.headers.Authorization = `Bearer ${accessToken}`;
-                return options;
+              }
+              return options;
             },
         })
     );
