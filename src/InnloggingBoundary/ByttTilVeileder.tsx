559b0b93b322a665cbce5cb7e6a4a6f2ed000719
havardmj <havard.myrvold.johannessen@nav.no>
2022-09-07 00:08:37 +0200

    fikset par styling feil paa besluttervisning

--- src/InnloggingBoundary/ByttTilVeileder.tsx
+++ src/InnloggingBoundary/ByttTilVeileder.tsx
@@ -2,27 +2,24 @@ import React, { FunctionComponent } from 'react';
 import { useCookies } from 'react-cookie';
 import { INNLOGGET_PART } from '@/RedirectEtterLogin';
 import { Knapp } from 'nav-frontend-knapper';
+import './byttTilBeslutter/byttTilBeslutter.less';
+import BEMHelper from '@/utils/bem';
 
 const ByttTilVeileder: FunctionComponent = () => {
     const [, setCookie] = useCookies([INNLOGGET_PART]);
+    const cls = BEMHelper('byttTilBeslutter');
 
     const onClick = () => {
         setCookie(INNLOGGET_PART, 'VEILEDER', { path: '/tiltaksgjennomforing' });
         window.location.href = '/tiltaksgjennomforing';
     };
     return (
-        <div
-            style={{
-                width: '100%',
-                backgroundColor: 'white',
-                display: 'flex',
-                justifyContent: 'center',
-                padding: '0.5rem',
-            }}
-        >
-            <Knapp mini onClick={onClick}>
-                Bytt til visning for veileder
-            </Knapp>
+        <div className={cls.className}>
+            <div className={cls.element('container')}>
+                <Knapp mini onClick={onClick}>
+                    Bytt til visning for veileder
+                </Knapp>
+            </div>
         </div>
     );
 };
