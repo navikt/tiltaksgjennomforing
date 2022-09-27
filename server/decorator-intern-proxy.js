1bcb125d696e0f7df60a419657f6d80ae86b7317
havardmj <havard.myrvold.johannessen@nav.no>
2022-09-27 23:52:08 +0200

    oppdatert decorator proxy

--- server/decorator-intern-proxy.js
+++ server/decorator-intern-proxy.js
@@ -1,6 +1,21 @@
-const setup = (app) => {
-    app.use('/internarbeidsflatedecorator', (req, res) => {
-        res.redirect(process.env.DECORATOR_INTERNAL + req.originalUrl);
-    });
+import axios from 'axios';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+const onbehalfof = require('./azure');
+const asyncHandler = require('express-async-handler');
+
+const setup = (app, azureClient, azureTokenEndpoint) => {
+    app.use(
+        '/modiacontextholder/api/decorator',
+        asyncHandler(async (req, res) => {
+            const accessToken = await onbehalfof.getOnBehalfOfAccessToken(azureClient, azureTokenEndpoint, req);
+            return await axios.get(`${process.env.DECORATOR_INTERNAL}/modiacontextholder/api/decorator`, {
+                headers: { ...req.headers, Authorization: `Bearer ${accessToken}` },
+            });
+        })
+    );
+    app.use(
+        '/internarbeidsflatedecorator',
+        createProxyMiddleware({ target: process.env.DECORATOR_INTERNAL, changeOrigin: true })
+    );
 };
 export default { setup };
