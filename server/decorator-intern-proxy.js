ccde4d4d32000e62f83057037ab42f78bd364fa2
Mattias Lundmark <lundmark.mattias@gmail.com>
2022-09-28 13:12:43 +0200

    Proxy fra modia request for innlogget bruker

--- server/decorator-intern-proxy.js
+++ server/decorator-intern-proxy.js
@@ -9,12 +9,14 @@ const setup = (app, azureClient, azureTokenEndpoint) => {
         '/modiacontextholder/api/decorator',
         asyncHandler(async (req, res) => {
             const accessToken = await onbehalfof.getOnBehalfOfAccessToken(azureClient, azureTokenEndpoint, req);
-            const response = await axios.get(`${process.env.DECORATOR_INTERNAL}/modiacontextholder/api/decorator`, {
+            console.log("Henter innlogget bruker til modia");
+            const response = await axios.get(`${process.env.APIGW_URL}/tiltaksgjennomforing-api/innlogget-bruker`, {
                 headers: { ...req.headers, Authorization: `Bearer ${accessToken}` },
             });
-            res.json({ ...response.data });
+            res.json({ ...response.data, ident: response.data.identifikator || '' });
         })
     );
+
     app.use(
         '/internarbeidsflatedecorator', (req, res) => {
             res.redirect(process.env.DECORATOR_INTERNAL + req.originalUrl);
