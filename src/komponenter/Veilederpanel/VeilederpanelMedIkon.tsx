a10b30345a833625f7e28da517c45ba7a76cee77
MagnusRom <magnus.skalleberg.rom@nav.no>
2022-10-03 13:43:06 +0200

    fjernet nav-frontend-veilederpanel

--- src/komponenter/Veilederpanel/VeilederpanelMedIkon.tsx
+++ src/komponenter/Veilederpanel/VeilederpanelMedIkon.tsx
@@ -1,12 +1,10 @@
-import Veilederpanel from 'nav-frontend-veilederpanel';
+import { GuidePanel } from '@navikt/ds-react';
 import * as React from 'react';
 import './VeilederpanelMedIkon.less';
 
 const VeilederpanelMedIkon = (props: { svg: React.ReactNode; children?: React.ReactNode }) => (
     <div className="veilederpanel-med-ikon">
-        <Veilederpanel svg={props.svg} kompakt={true} type="plakat">
-            {props.children}
-        </Veilederpanel>
+        <GuidePanel poster>{props.children}</GuidePanel>
     </div>
 );
 
