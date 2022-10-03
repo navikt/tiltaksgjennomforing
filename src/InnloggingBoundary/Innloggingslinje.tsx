1178ff62b737da0d9440ea4b0cd456a3f69aa413
MagnusRom <magnus.skalleberg.rom@nav.no>
2022-10-03 11:19:57 +0200

    Test

--- src/InnloggingBoundary/Innloggingslinje.tsx
+++ src/InnloggingBoundary/Innloggingslinje.tsx
@@ -45,6 +45,7 @@ const Innloggingslinje: FunctionComponent<Props> = (props) => {
                                 </div>
                             </Accordion.Header>
                             <Accordion.Content>
+                                <p>Test/p>
                                 <div className={cls.className}>
                                     <LoggUtKnapp />
                                 </div>
