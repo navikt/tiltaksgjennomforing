f11acf57f902038854c28acb65a4469b2dd18bb0
Henrietta E. Bleness <h.bleness@gmail.com>
2022-11-09 10:04:59 +0100

    Endret "Tilbake til oversikten" til Knapp (#1095)
    Gjorde dette slik at man kan bruke useHistory til å navigere og huske søkefeltet fra "Tiltaksoversiktsiden"
    
    Co-authored-by: Catriona Tørklep <catriona.torklep@nav.no>
    
    Co-authored-by: Catriona Tørklep <catriona.torklep@nav.no>

--- src/AvtaleSide/TilbakeTilOversiktLenke/TilbakeTilOversiktLenke.tsx
+++ src/AvtaleSide/TilbakeTilOversiktLenke/TilbakeTilOversiktLenke.tsx
@@ -1,9 +1,9 @@
-import { pathTilOversikt } from '@/paths';
 import { Back } from '@navikt/ds-icons';
-import React, { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
+import { FunctionComponent } from 'react';
+import { useHistory } from 'react-router-dom';
 import './TilbakeTilOversiktLenke.less';
 import BEMHelper from '@/utils/bem';
+import { Button } from '@navikt/ds-react';
 
 type Props = {
     onClick?: () => void;
@@ -12,18 +12,24 @@ type Props = {
 
 const TilbakeTilOversiktLenke: FunctionComponent<Props> = (props) => {
     const cls = BEMHelper('tilbaketiloversikt');
+    const history = useHistory();
+
+    const handleOnClick = () => {
+        if (props.onClick) {
+            props.onClick();
+        }
+        history.goBack();
+    };
+
     return (
-        <Link
-            to={{ pathname: pathTilOversikt, search: window.location.search }}
-            className={cls.element('lenke')}
-            onClick={props.onClick}
-            role="menuitem"
+        <Button
+            variant="tertiary"
+            onClick={handleOnClick}
+            icon={<Back className={cls.element('chevron')} />}
+            iconPosition="left"
         >
-            <div aria-hidden={true}>
-                <Back className={cls.element('chevron')} />
-            </div>
             {props.tekst || 'Tilbake til oversikt'}
-        </Link>
+        </Button>
     );
 };
 
