8758fb9a920235ddd575f6629d6820707104a585
MagnusRom <magnus.skalleberg.rom@nav.no>
2022-11-24 13:59:19 +0100

    Oppdatert typografi i Gjennomføres.tsx

--- src/AvtaleSide/AvtaleStatus/Gjennomføres.tsx
+++ src/AvtaleSide/AvtaleStatus/Gjennomføres.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as CheckIkon } from '@/assets/ikoner/check.svg';
 import StatusPanel from '@/AvtaleSide/AvtaleStatus/StatusPanel';
 import { Avtale } from '@/types/avtale';
 import { formatterDato, NORSK_DATO_FORMAT } from '@/utils/datoUtils';
-import { Normaltekst } from 'nav-frontend-typografi';
+import { BodyShort } from '@navikt/ds-react';
 import React, { FunctionComponent } from 'react';
 
 interface Props {
@@ -15,10 +15,10 @@ const Gjennomføres: FunctionComponent<Props> = ({ avtale }) => {
             ikon={CheckIkon}
             header="Tiltaket gjennomføres"
             body={
-                <Normaltekst>
+                <BodyShort size="small">
                     Avtale ble inngått {formatterDato(avtale.avtaleInngått!, NORSK_DATO_FORMAT)}. Tiltaket startet{' '}
                     {formatterDato(avtale.gjeldendeInnhold.startDato!, NORSK_DATO_FORMAT)}.
-                </Normaltekst>
+                </BodyShort>
             }
         />
     );
