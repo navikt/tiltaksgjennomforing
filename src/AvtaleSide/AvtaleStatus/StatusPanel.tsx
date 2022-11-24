61446f8c6d855fd81e4576328c58fc86bb3b1e48
MagnusRom <magnus.skalleberg.rom@nav.no>
2022-11-24 14:13:29 +0100

    Oppdatert typografi i StatusPanel.tsx

--- src/AvtaleSide/AvtaleStatus/StatusPanel.tsx
+++ src/AvtaleSide/AvtaleStatus/StatusPanel.tsx
@@ -1,7 +1,7 @@
 import React, { SVGProps } from 'react';
 import Innholdsboks from '@/komponenter/Innholdsboks/Innholdsboks';
 import VerticalSpacer from '@/komponenter/layout/VerticalSpacer';
-import { Innholdstittel, Systemtittel } from 'nav-frontend-typografi';
+import { Heading } from '@navikt/ds-react';
 
 const StatusPanel: React.FunctionComponent<{
     ikon: React.ComponentType<SVGProps<any>>;
@@ -14,7 +14,11 @@ const StatusPanel: React.FunctionComponent<{
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                 <Ikon style={{ height: '40px', width: '40px' }} />
                 <VerticalSpacer rem={1} />
-                {header.length > 36 ? <Systemtittel>{header}</Systemtittel> : <Innholdstittel>{header}</Innholdstittel>}
+                {header.length > 36 ? (
+                    <Heading size="medium">{header}</Heading>
+                ) : (
+                    <Heading size="large">{header}</Heading>
+                )}
             </div>
 
             <VerticalSpacer rem={1} />
