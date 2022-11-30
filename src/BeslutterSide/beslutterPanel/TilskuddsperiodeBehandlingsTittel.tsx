19218599225865f5e904fdc7e54abb13f5ce474b
MagnusRom <magnus.skalleberg.rom@nav.no>
2022-11-30 14:00:27 +0100

    Oppdatert typografi

--- src/BeslutterSide/beslutterPanel/TilskuddsperiodeBehandlingsTittel.tsx
+++ src/BeslutterSide/beslutterPanel/TilskuddsperiodeBehandlingsTittel.tsx
@@ -1,29 +1,28 @@
-import React, {FunctionComponent, useContext} from "react";
-import {Normaltekst, Undertittel} from "nav-frontend-typografi";
-import {TilskuddPeriodeStatus} from "@/types/avtale";
-import BEMHelper from "@/utils/bem";
-import {AvtaleContext} from "@/AvtaleProvider";
-import {formatterDato, NORSK_DATO_FORMAT} from "@/utils/datoUtils";
-
+import React, { FunctionComponent, useContext } from 'react';
+import { BodyShort, Heading } from '@navikt/ds-react';
+import { TilskuddPeriodeStatus } from '@/types/avtale';
+import BEMHelper from '@/utils/bem';
+import { AvtaleContext } from '@/AvtaleProvider';
+import { formatterDato, NORSK_DATO_FORMAT } from '@/utils/datoUtils';
 
 const TilskuddsperiodeBehandlingsTittel: FunctionComponent = () => {
     const cls = BEMHelper('beslutter-panel');
-    const { avtale } = useContext(AvtaleContext)
+    const { avtale } = useContext(AvtaleContext);
 
-    if(!avtale.gjeldendeTilskuddsperiode) return null;
+    if (!avtale.gjeldendeTilskuddsperiode) return null;
 
     const tittel: { [key in TilskuddPeriodeStatus]: string } = {
         AVSLÅTT: 'Tilskuddsperiode er avslått',
         GODKJENT: 'Tilskuddsperiode er godkjent',
         UBEHANDLET: 'Tilskuddsperiode som skal godkjennes',
-        ANNULLERT: 'Tilskuddsperiode er annullert'
+        ANNULLERT: 'Tilskuddsperiode er annullert',
     };
 
     return (
         <div className={cls.element('tittel')}>
-            <Undertittel>{tittel[avtale.gjeldendeTilskuddsperiode.status]}</Undertittel>
-            <Normaltekst>{formatterDato(avtale.opprettetTidspunkt, NORSK_DATO_FORMAT)}</Normaltekst>
+            <Heading size="small">{tittel[avtale.gjeldendeTilskuddsperiode.status]}</Heading>
+            <BodyShort size="small">{formatterDato(avtale.opprettetTidspunkt, NORSK_DATO_FORMAT)}</BodyShort>
         </div>
-    )
-}
+    );
+};
 export default TilskuddsperiodeBehandlingsTittel;
