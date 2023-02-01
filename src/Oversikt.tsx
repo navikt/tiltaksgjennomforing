1fbb6dd76a39e122fc2fd10c394f220235468eb2
Sindre Dahl Løken <sindre.loken@nav.no>
2023-02-01 09:53:59 +0100

    midlertidig skrudd av beslutteroversikten

--- src/Oversikt.tsx
+++ src/Oversikt.tsx
@@ -1,14 +1,15 @@
-import React, { FunctionComponent, useContext } from 'react';
-import { InnloggetBrukerContext } from '@/InnloggingBoundary/InnloggingBoundary';
-import BeslutterOversikt from '@/BeslutterOversikt/BeslutterOversikt';
 import AvtaleOversikt from '@/AvtaleOversikt/AvtaleOversikt';
 import { FiltreringProvider } from '@/AvtaleOversikt/Filtrering/FiltreringProvider';
+import { InnloggetBrukerContext } from '@/InnloggingBoundary/InnloggingBoundary';
+import { Alert } from '@navikt/ds-react';
+import { FunctionComponent, useContext } from 'react';
 
 const Oversikt: FunctionComponent = () => {
     const innloggetBruker = useContext(InnloggetBrukerContext);
     return (
         <FiltreringProvider>
-            {innloggetBruker.rolle === 'BESLUTTER' ? <BeslutterOversikt /> : <AvtaleOversikt />}
+            {/* {innloggetBruker.rolle === 'BESLUTTER' ? <BeslutterOversikt /> : <AvtaleOversikt />} */}
+            {innloggetBruker.rolle === 'BESLUTTER' ? <Alert variant='info'>Vi jobber med å rette noen feil i besluttervisningen.</Alert> : <AvtaleOversikt />}
         </FiltreringProvider>
     );
 };
