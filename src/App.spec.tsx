851ec84af8d3b947475f3631117f335ef1dc821a
Millad Dagdoni <milladd@gmail.com>
2023-04-03 15:54:23 +0200

    jobbet med en <app/> test, sliter med å kjøre testen sammen med alle andre testene pararrelt
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 import App from './App';
 import InnloggingBoundary from './InnloggingBoundary/InnloggingBoundary';
 
 import { MemoryRouter } from 'react-router';
-import * as reactRouter from 'react-router-dom';
-import PropTypes from 'prop-types';
-/*
+import  * as rrd from 'react-router-dom';
+import PropTypes from "prop-types";
+
+const oldBrowserRoiuter = rrd.BrowserRouter
 const MockBrowserRouter = ({ children }) => (
     <MemoryRouter initialEntries={['/tiltaksgjennomforing/']}>
         { children }
     </MemoryRouter>
 );
-*/
+
+
+
 test('Test that <App> renders correctly', () => {
-    // MockBrowserRouter.propTypes = { children: PropTypes.node.isRequired };
-    //const oldBrowserRoiuter = reactRouter.BrowserRouter
-    //reactRouter.BrowserRouter = MockBrowserRouter;
+
+     MockBrowserRouter.propTypes = { children: PropTypes.node.isRequired };
+     rrd.BrowserRouter = MockBrowserRouter;
+
     const wrapper = mount(<App />);
-    //reactRouter.BrowserRouter = oldBrowserRoiuter;
+    const {BrowserRouter} = import("react-router-dom")
+    rrd.BrowserRouter = BrowserRouter
+    
     expect(wrapper).toHaveLength(1);
     const innloggingBoundary = wrapper.find(InnloggingBoundary);
-    console.log("inloggetBoundary", innloggingBoundary)
-    expect(innloggingBoundary).toHaveLength(0);
+    expect(innloggingBoundary).toHaveLength(1);
+
 });
