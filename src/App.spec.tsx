8ab0df013104d47f888f12edbf31efc1b1d232ff
Millad Dagdoni <milladd@gmail.com>
2023-04-05 09:50:37 +0200

    forenklet test for App.spec.tsx
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,32 +1,23 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import App from './App';
 import InnloggingBoundary from './InnloggingBoundary/InnloggingBoundary';
-
+import AvtaleOversikt from "@/AvtaleOversikt/AvtaleOversikt";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+import { createMemoryHistory } from 'history';
 import { MemoryRouter } from 'react-router';
-import  * as rrd from 'react-router-dom';
-import PropTypes from "prop-types";
-
-const oldBrowserRoiuter = rrd.BrowserRouter
-const MockBrowserRouter = ({ children }) => (
-    <MemoryRouter initialEntries={['/tiltaksgjennomforing/']}>
-        { children }
-    </MemoryRouter>
-);
-
+import Informasjonsside from "@/Informasjonsside/Informasjonsside";
+import {pathTilInformasjonssideUinnlogget} from "@/paths";
 
 
 test('Test that <App> renders correctly', () => {
 
-     MockBrowserRouter.propTypes = { children: PropTypes.node.isRequired };
-     rrd.BrowserRouter = MockBrowserRouter;
+    const history = createMemoryHistory({ initialEntries: ['/tiltaksgjennomforing/'] });
 
-    const wrapper = mount(<App />);
-    const {BrowserRouter} = import("react-router-dom")
-    rrd.BrowserRouter = BrowserRouter
-    
+    const wrapper = shallow(
+            <App />
+    );
     expect(wrapper).toHaveLength(1);
-    const innloggingBoundary = wrapper.find(InnloggingBoundary);
-    expect(innloggingBoundary).toHaveLength(1);
-
+    const routesRenderer = wrapper.find(Routes);
+    expect(routesRenderer).toHaveLength(1);
 });
