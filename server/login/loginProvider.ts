7c0cbfa37a260d961833c03d4e6890b6b17d867f
havardmj <havard.myrvold.johannessen@nav.no>
2023-04-13 09:34:16 +0200

    wip

--- server/login/loginProvider.ts
+++ server/login/loginProvider.ts
@@ -1,45 +1,43 @@
-import { getMiljø, Miljø } from "../paths/miljø";
-import labsProxy from "../proxy/labs-proxy";
-import { BaseClient } from "openid-client";
-import azure from "./azure";
-import apiProxy from "../proxy/api-proxy";
-import decoratorInternProxy from "../proxy/decorator-intern-proxy";
-import tokenx from "./tokenx";
-import notifikasjonProxy from "../proxy/notifikasjoner-proxy";
-import decoratorEksternProxy from "../proxy/decorator-ekstern-proxy";
-import { Express } from "express";
-
+import { getMiljø, Miljø } from '../paths/miljø';
+import labsProxy from '../proxy/labs-proxy';
+import { BaseClient } from 'openid-client';
+import azure from './azure';
+import apiProxy from '../proxy/api-proxy';
+import decoratorInternProxy from '../proxy/decorator-intern-proxy';
+import tokenx from './tokenx';
+import notifikasjonProxy from '../proxy/notifikasjoner-proxy';
+import decoratorEksternProxy from '../proxy/decorator-ekstern-proxy';
+import { Express } from 'express';
 
 const miljo: Miljø = getMiljø();
 const setupOauth2Clients = async (server: Express) => {
-  if (miljo === Miljø.DEV_GCP || miljo === Miljø.PROD_GCP) {
-    if (process.env.INTERN_INGRESS) {
-      return await initLoginAndProxyForInternal(server);
+    if (miljo === Miljø.DEV_GCP || miljo === Miljø.PROD_GCP) {
+        if (process.env.INTERN_INGRESS) {
+            return await initLoginAndProxyForInternal(server);
+        }
+        return await initLoginAndProxyForExternal(server);
     }
-    return await initLoginAndProxyForExternal(server);
-  }
-  return labsProxy.setup(server);
-
+    return labsProxy.setup(server);
 };
 
 async function initLoginAndProxyForInternal(server: Express): Promise<void> {
-  const client: BaseClient = await azure.client();
-  const tokenEndpoint = await azure.azureTokenEndpoint();
+    const client: BaseClient = await azure.client();
+    const tokenEndpoint = await azure.azureTokenEndpoint();
 
-  apiProxy.azureSetup(server, client, tokenEndpoint);
-  decoratorInternProxy.setup(server, client, tokenEndpoint);
+    apiProxy.azureSetup(server, client, tokenEndpoint);
+    decoratorInternProxy.setup(server, client, tokenEndpoint);
 
-  console.log('Satt opp api-proxy med azure obh');
+    console.log('Satt opp api-proxy med azure obh');
 }
 
 async function initLoginAndProxyForExternal(server: Express): Promise<void> {
-  const tokenxAuthClient = await tokenx.client();
+    const tokenxAuthClient = await tokenx.client();
 
-  apiProxy.tokenxSetup(server, tokenxAuthClient);
-  notifikasjonProxy.setup(server, tokenxAuthClient);
-  decoratorEksternProxy.setup(server);
+    apiProxy.tokenxSetup(server, tokenxAuthClient);
+    // notifikasjonProxy.setup(server, tokenxAuthClient);
+    // decoratorEksternProxy.setup(server);
 
-  console.log('Satt opp api-proxy med tokenx');
+    console.log('Satt opp api-proxy med tokenx');
 }
 
-export default { setupOauth2Clients }
+export default { setupOauth2Clients };
