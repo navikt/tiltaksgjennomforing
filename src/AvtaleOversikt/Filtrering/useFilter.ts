f388cacb7fe87cb214be42b5721d2fea74e99100
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2023-05-16 10:48:39 +0200

    Nullstill til side 1 ved filterendringer

--- src/AvtaleOversikt/Filtrering/useFilter.ts
+++ src/AvtaleOversikt/Filtrering/useFilter.ts
@@ -13,27 +13,19 @@ export const useFilter = () => {
     useEffect(() => {
         const newParams = toObject(searchParams);
         if (!_.isEqual(newParams, filtre)) {
-            console.log("Sett params");
             setFiltre(newParams);
         }
     }, [searchParams, setFiltre, filtre]);
-
-    // const endreFilter = useCallback(
-    //     (endring: Filtrering) => {
-    //         const nyeFiltre: any = { ...filtre, ...endring };
-    //         Object.keys(nyeFiltre).forEach((key: string) => !nyeFiltre[key] && delete nyeFiltre[key]);
-    //         navigate('?' + new URLSearchParams(nyeFiltre).toString());
-    //         parseWindowLocationSearch();
-    //     },
-    //     [filtre, navigate, parseWindowLocationSearch]
-    // );
 
     const endreFilter = (endring: Filtrering) => {
         // Type 'any' fordi Object.keys ikke skjønner at 'k' er en key av Filtrering
         const nyeFiltre: any = { ...filtre, ...endring };
         Object.keys(nyeFiltre).forEach((k) => !nyeFiltre[k] && delete nyeFiltre[k]);
-        console.log(nyeFiltre);
         if (!_.isEqual(nyeFiltre, filtre)) {
+            // For alle filtreringer som ikke er en page-endring, nullstill til "side 1"
+            if (!endring.page) {
+                delete nyeFiltre['page'];
+            }
             setSearchParams(nyeFiltre);
         }
     };
