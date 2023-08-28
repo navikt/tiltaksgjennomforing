8bfa973fb39cb07ac0062ebbb95687fed3a4950e
Millad Dagdoni <milladd@gmail.com>
2023-08-28 13:52:36 +0200

    prøver med start og til dato
    Co-authored-by: Magnus Skalleberg Rom <magnus.skalleberg.rom@nav.no>

--- src/komponenter/datovelgerRange/DatovelgerRange.tsx
+++ src/komponenter/datovelgerRange/DatovelgerRange.tsx
@@ -1,65 +1,52 @@
 import { AvtaleMinMaxDato } from '@/AvtaleSide/steg/VarighetSteg/AvtaleMinMaxDato/AvtaleMinMaxDato';
-import { DatePicker, useRangeDatepicker } from '@navikt/ds-react';
+import {DatePicker, useDatepicker, useRangeDatepicker} from '@navikt/ds-react';
 import format from 'date-fns/format';
 import nbLocale from 'date-fns/locale/nb';
-import { useState } from 'react';
+import {ChangeEvent, ChangeEventHandler, useState} from 'react';
 import Nytt from '../NyttIAppen/Nytt';
+import {formatterDatoHvisDefinert} from "@/utils/datoUtils";
 
 const DatovelgerRange = () => {
     const fjernTid = (timestamp: string) => timestamp.split('T')[0];
 
     const [hasError, setHasError] = useState<boolean>(false);
 
-    const [nyTilDato, setNyTilDato] = useState<Date | undefined>(new Date(fjernTid(AvtaleMinMaxDato(true).minDate || '')));
-    const [nyFraDato, setNyFraDato] = useState<Date | undefined>(new Date(fjernTid(AvtaleMinMaxDato(true).maxDate || '')));
+    const [fraDato, setFraDato] = useState<Date | undefined>(new Date(fjernTid(AvtaleMinMaxDato(true).minDate || '')));
+    const [tilDato, setTilDato] = useState<Date | undefined>(new Date(fjernTid(AvtaleMinMaxDato(true).maxDate || '')));
 
-    //new Date( fjernTid(AvtaleMinMaxDato(true).minDate ||
 
-    const { datepickerProps, toInputProps, fromInputProps, selectedRange } = useRangeDatepicker({
-        fromDate: nyTilDato,
-        
-        onValidate: (val) => {
-            if(!val.to.isValidDate)
 
-            return setHasError(true);
+    const { datepickerProps, inputProps } = useDatepicker({
+        fromDate: fraDato,
+        toDate: tilDato,
+        inputFormat: 'dd.MM.yyyy',
+
+
+
+        onDateChange: (dato) => {
+            console.log(dato, inputProps)
         },
-        onRangeChange : (rangeval) => {
-            if(rangeval && (rangeval.to && rangeval.from != null)){
-                if(rangeval?.from?.getTime() - new Date(rangeval.to).getTime()){
-                    console.log("tesrt  tesr ")
-                }
-            }
-        }
 
-        //var time = new Date().getTime() - new Date("2013-02-20T12:01:04.753Z").getTime();        
-        
     });
-
 
     return (
         <>
-            <DatePicker {...datepickerProps}>
+            <DatePicker {...datepickerProps} id={"FRA"} className={"FRA"}  >
                 <DatePicker.Input
-                    {...fromInputProps}
                     placeholder="dd.mm.åååå"
-                    //error={hasError && feilmeldingTekst}
                     label="Fra"
                     error={hasError && 'Feil fra'}
                 />
+            </DatePicker>
+            <DatePicker {...datepickerProps} id={"TO"} className={"TO"} >
+
                 <DatePicker.Input
-                    {...toInputProps}
                     placeholder="dd.mm.åååå"
-                    //error={hasError && feilmeldingTekst}
                     label="Til"
                     error={hasError && 'Feil til'}
                 />
             </DatePicker>
-            {selectedRange && (
-                <div className="pt-4">
-                    <div>{selectedRange?.from && format(selectedRange.from, 'dd.MM.yyyy', { locale: nbLocale })}</div>
-                    <div>{selectedRange?.to && format(selectedRange.to, 'dd.MM.yyyy', { locale: nbLocale })}</div>
-                </div>
-            )}
+
         </>
     );
 };
