8f36a505c0214c40199b21c1091e4624278131e8
Mickel Hoang m1 <mickel.hoang@bekk.no>
2023-11-24 12:36:30 +0100

    Slettet menysortering
    Co-authored-by: Odd Andreas Sørsæther <oddsor@users.noreply.github.com>

--- src/AvtaleOversikt/Filtrering/VeilederFiltrering.tsx
+++ src/AvtaleOversikt/Filtrering/VeilederFiltrering.tsx
@@ -1,7 +1,6 @@
 import { DeltakerOgBedriftFilter } from '@/AvtaleOversikt/Filtrering/DeltakerOgBedriftFilter';
 import StatusFilter from '@/AvtaleOversikt/Filtrering/StatusFilter';
 import TiltakstypeFilter from '@/AvtaleOversikt/Filtrering/TiltakstypeFilter';
-import Sortering from '@/AvtaleOversikt/Sortering';
 import BEMHelper from '@/utils/bem';
 import React, { FunctionComponent } from 'react';
 import './Filtrering.less';
@@ -11,7 +10,6 @@ const cls = BEMHelper('filtrering');
 const VeilederFiltrering: FunctionComponent = () => {
     return (
         <div className={cls.className}>
-            <Sortering />
             <DeltakerOgBedriftFilter />
             <TiltakstypeFilter erBeslutter={false} />
             <StatusFilter />
