75361731c5505efbf1c222fa50ca8d85f463f520
Mattias Lundmark <lundmark.mattias@gmail.com>
2023-12-19 14:50:04 +0100

    Sjekker, og advarer, om beløpet er urimelig høyt

--- src/AvtaleSide/steg/BeregningTilskudd/Manedslonn.tsx
+++ src/AvtaleSide/steg/BeregningTilskudd/Manedslonn.tsx
@@ -3,6 +3,7 @@ import { Column, Row } from '@/komponenter/NavGrid/Grid';
 import ValutaInput from '@/komponenter/form/ValutaInput';
 import { BEMWrapper } from '@/utils/bem';
 import { AvtaleContext } from '@/AvtaleProvider';
+import { Alert } from '@navikt/ds-react';
 
 interface Props {
     cls: BEMWrapper;
@@ -10,6 +11,8 @@ interface Props {
 
 const Manedslonn: React.FC<Props> = ({ cls }: Props) => {
     const { avtale, settAvtaleInnholdVerdier, settOgKalkulerBeregningsverdier } = useContext(AvtaleContext);
+    const erHøyManedslønn = (avtale.gjeldendeInnhold.manedslonn !== undefined && avtale.gjeldendeInnhold.manedslonn > 99999);
+
     return (
         <Row className={cls.element('rad', 'input')}>
             <Column md="6" className={cls.element('valuta-input')}>
@@ -26,6 +29,11 @@ const Manedslonn: React.FC<Props> = ({ cls }: Props) => {
                     onBlur={(event) => settOgKalkulerBeregningsverdier({ manedslonn: parseFloat(event.target.value) })}
                     min={0}
                 />
+                {erHøyManedslønn &&
+                    <Alert variant="warning" className={cls.element('alert')}>
+                        Er du sikker på at dette er riktig månedslønn?
+                    </Alert>
+                }
             </Column>
         </Row>
     );
