a9721d156d428d6d18bad6ecff51623cf6fe7538
Sindre Dahl Løken <sindre.loken@nav.no>
2024-02-23 16:19:08 +0100

    prøver noe mer greier

--- server/src/paths/whitelistPaths.ts
+++ server/src/paths/whitelistPaths.ts
@@ -1,1 +1,8 @@
-export const whitelistPaths = ['feature'];
+const paths = ['/feature'];
+
+const whitelistPaths: Array<string> = [];
+paths.forEach((path) => {
+    whitelistPaths.push((path = '/tiltaksgjennomforing/api' + path));
+});
+
+export default whitelistPaths;
