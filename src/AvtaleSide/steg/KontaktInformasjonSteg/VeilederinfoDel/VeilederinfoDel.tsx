507f1379954b217c6dec71ed5e616868e1b3ba7e
d142796 <millad.dagdoni@nav.no>
2024-03-18 13:02:10 +0100

    Forenklet visning av ulike statuser for eier av avtale i kontaktinfo steg for veiledere. Det blir nå enklere for veiledere å se hvem som eier avtalen. De får en varsel om at de må trykke på overtal avtalen og skrive inn riktig kontakt info på avtalen.

--- src/AvtaleSide/steg/KontaktInformasjonSteg/VeilederinfoDel/VeilederinfoDel.tsx
+++ src/AvtaleSide/steg/KontaktInformasjonSteg/VeilederinfoDel/VeilederinfoDel.tsx
@@ -6,37 +6,62 @@ import TelefonnummerInput from '@/komponenter/TelefonnummerInput/TelefonnummerIn
 import BEMHelper from '@/utils/bem';
 import React, { useContext } from 'react';
 import { InnloggetBrukerContext } from '@/InnloggingBoundary/InnloggingBoundary';
+import { Alert } from '@navikt/ds-react';
 
 const VeilederinfoDel = () => {
     const cls = BEMHelper('kontaktinfo');
-    const avtaleContext = useContext(AvtaleContext);
-    const { navn, rolle } = useContext(InnloggetBrukerContext);
+    const { avtale, settAvtaleInnholdVerdi } = useContext(AvtaleContext);
+    const { navn, rolle, identifikator } = useContext(InnloggetBrukerContext);
 
     return (
         <>
             <div className={cls.element('container')}>
                 <SkjemaTittel>Kontaktperson i NAV</SkjemaTittel>
-                {rolle === 'VEILEDER' && navn?.trim().length !== 0 && (
-                    <SkjemaTittel>Veileder navn: {navn}</SkjemaTittel>
+                {rolle === 'VEILEDER' && (
+                    <>
+                        {avtale.veilederNavIdent && avtale.veilederNavIdent === identifikator && (
+                            <p>
+                                Eier av avtalen er{' '}
+                                <b>
+                                    <u>{identifikator}</u>
+                                </b>
+                                .
+                            </p>
+                        )}
+                        {avtale.veilederNavIdent?.trim().length > 0 && avtale.veilederNavIdent !== identifikator && (
+                            <p>
+                                Eier av avtalen er{' '}
+                                <b>
+                                    <u>{avtale.veilederNavIdent}</u>
+                                </b>
+                                .
+                            </p>
+                        )}
+                        {!avtale.veilederNavIdent && <Alert variant={'warning'}>Det er ingen eier av avtalen.</Alert>}
+                        <p>
+                            For å overta avtalen må du eller en ny veileder gå til menyen og velge "Overta avtale" i
+                            tillegg til å skrive inn navn og telefonnummer her.
+                        </p>
+                    </>
                 )}
                 <div className={cls.element('rad')}>
                     <PakrevdInput
                         label="Fornavn"
-                        verdi={avtaleContext.avtale.gjeldendeInnhold.veilederFornavn}
-                        settVerdi={(verdi) => avtaleContext.settAvtaleInnholdVerdi('veilederFornavn', verdi)}
+                        verdi={avtale.gjeldendeInnhold.veilederFornavn}
+                        settVerdi={(verdi) => settAvtaleInnholdVerdi('veilederFornavn', verdi)}
                     />
                     <PakrevdInput
                         label="Etternavn"
-                        verdi={avtaleContext.avtale.gjeldendeInnhold.veilederEtternavn}
-                        settVerdi={(verdi) => avtaleContext.settAvtaleInnholdVerdi('veilederEtternavn', verdi)}
+                        verdi={avtale.gjeldendeInnhold.veilederEtternavn}
+                        settVerdi={(verdi) => settAvtaleInnholdVerdi('veilederEtternavn', verdi)}
                     />
                 </div>
                 <VerticalSpacer rem={1} />
                 <div className={cls.element('rad')}>
                     <TelefonnummerInput
                         label="Mobilnummer"
-                        verdi={avtaleContext.avtale.gjeldendeInnhold.veilederTlf}
-                        settVerdi={(verdi) => avtaleContext.settAvtaleInnholdVerdi('veilederTlf', verdi)}
+                        verdi={avtale.gjeldendeInnhold.veilederTlf}
+                        settVerdi={(verdi) => settAvtaleInnholdVerdi('veilederTlf', verdi)}
                     />
                 </div>
             </div>
