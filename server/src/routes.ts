84279086513dfc2f5965ce51b3fdf804b38ad748
Andreas Nordahl <andnorda@gmail.com>
2024-04-02 13:38:54 +0200

    Ta i bruk @navikt/oasis (#1204)
    * use @navikt/oasis
    
    * add missing files. oopsie.
    
    * Intern vs ekstern ingress
    
    TokenX-konfigurasjon virket ikke
    
    * Ikke kjør labsproxy "no matter what"
    
    ---------
    
    Co-authored-by: Odd Andreas Sørsæther <oddsor@users.noreply.github.com>

---
+++ server/src/routes.ts
@@ -0,0 +1,24 @@
+import { getMiljo, Miljo } from './paths/miljo';
+import labsProxy from './proxy/labs-proxy';
+import apiProxy from './proxy/api-proxy';
+import decoratorInternProxy from './proxy/decorator-intern-proxy';
+import notifikasjonProxy from './proxy/notifikasjoner-proxy';
+import decoratorEksternProxy from './proxy/decorator-ekstern-proxy';
+import { Express } from 'express';
+
+export const setupRoutes = async (server: Express) => {
+    const miljo: Miljo = getMiljo();
+
+    if (miljo === Miljo.DEV_GCP || miljo === Miljo.PROD_GCP) {
+        if (process.env.INTERN_INGRESS) {
+            apiProxy.azureSetup(server);
+            decoratorInternProxy.setup(server);
+        } else {
+            apiProxy.tokenxSetup(server);
+            notifikasjonProxy.setup(server);
+            decoratorEksternProxy.setup(server);
+        }
+    } else {
+        labsProxy.setup(server);
+    }
+};
