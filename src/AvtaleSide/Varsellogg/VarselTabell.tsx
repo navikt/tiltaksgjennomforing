ba180deb35ca81a2061d8a53c82e01ba35199c10
MagnusRom <65652609+MagnusRom@users.noreply.github.com>
2024-05-28 11:36:34 +0200

    Hendelselogg oppdatering (#1222)
    * lagt til veileder ident når man er logget inn som saksbehandler
    
    * fikset visning feil
    
    * opprydding

--- src/AvtaleSide/Varsellogg/VarselTabell.tsx
+++ src/AvtaleSide/Varsellogg/VarselTabell.tsx
@@ -97,11 +97,7 @@ const VarselTabell: FunctionComponent<Props> = (props) => {
                                     </div>
                                 </Table.DataCell>
                                 <Table.DataCell role="cell" aria-labelledby="utført_av">
-                                    {[
-                                        'TILSKUDDSPERIODE_GODKJENT',
-                                        'TILSKUDDSPERIODE_AVSLATT',
-                                        'GODKJENT_FOR_ETTERREGISTRERING',
-                                    ].includes(varsel.hendelseType) ? (
+                                    {varsel.mottaker === 'VEILEDER' || varsel.mottaker === 'BESLUTTER' ? (
                                         <UtgråetTekst grå={varsel.skjules}>
                                             {storForbokstav(
                                                 varsel.utførtAvIdentifikator !== null
