acc77890ef9fdeed15b0dc882551d3fd703c0566
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-06-18 15:16:53 +0200

    Skjul VTAO-filter via feature-toggle

--- src/AvtaleOversikt/Filtrering/TiltakstypeFilter.tsx
+++ src/AvtaleOversikt/Filtrering/TiltakstypeFilter.tsx
@@ -1,13 +1,16 @@
 import { Filter } from '@/AvtaleOversikt/Filtrering/Filter';
 import { useFilter } from '@/AvtaleOversikt/Filtrering/useFilter';
+import { Feature, FeatureToggleContext } from '@/FeatureToggleProvider';
 import { OptionProps } from '@/komponenter/form/SelectInput';
 import { TiltaksType } from '@/types/avtale';
 import { Radio, RadioGroup } from '@navikt/ds-react';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useContext } from 'react';
 
 export type FiltreringMedBeslutterProps = { erBeslutter: boolean };
 const TiltakstypeFilter: FunctionComponent<FiltreringMedBeslutterProps> = (props) => {
     const { endreFilter, filtre } = useFilter();
+    const contex = useContext(FeatureToggleContext);
+    const variant = contex[Feature.VtaoTiltakToggle];
 
     const alleTiltakstyperBeslutter: OptionProps[] = [
         { value: '', label: 'Alle' },
@@ -24,8 +27,8 @@ const TiltakstypeFilter: FunctionComponent<FiltreringMedBeslutterProps> = (props
         { value: 'SOMMERJOBB', label: 'Sommerjobb' },
         { value: 'MENTOR', label: 'Mentor' },
         { value: 'INKLUDERINGSTILSKUDD', label: 'Inkluderingstilskudd' },
-        { value: 'VTAO', label: 'VTA-oppfølging' },
-    ];
+        variant ? { value: 'VTAO', label: 'VTA-oppfølging' } : null,
+    ].filter((x) => x !== null) as OptionProps[];
 
     const tiltakstyper = props.erBeslutter ? alleTiltakstyperBeslutter : alleTiltakstyper;
 
