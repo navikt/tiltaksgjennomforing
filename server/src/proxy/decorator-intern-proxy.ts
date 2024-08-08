50aca1dee74c0e25acabd4d1a4af7fdacb92c2fb
Eirik Vigeland <eirik.vigeland@nav.no>
2024-08-08 14:06:39 +0200

    feat. Proxy modia redirect lenker til modiacontextholder med obo token (#1231)
    * feat. Proxy modia redirect lenker til modiacontextholder med obo token
    
    * fix. prod-fss -> prod-gcp for modiacontextholder
    
    * fix. fjernet innlogget part fra cookies til context holder

--- server/src/proxy/decorator-intern-proxy.ts
+++ server/src/proxy/decorator-intern-proxy.ts
@@ -1,6 +1,6 @@
 import proxy from 'express-http-proxy';
-import { Express, Response } from 'express';
-import { Request } from 'express-serve-static-core';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { Express, Response, Request } from 'express';
 import { ParsedQs } from 'qs';
 import { IncomingMessage, RequestOptions } from 'http';
 import { requestOboToken } from '../auth';
@@ -8,7 +8,7 @@ import { requestOboToken } from '../auth';
 const setup = (app: Express) => {
     app.use(
         '/modiacontextholder/api/decorator',
-        proxy(process.env.APIGW_URL as string, {
+        proxy(process.env.APIGW_URL, {
             proxyReqPathResolver: (req: Request<{}, any, any, ParsedQs, Record<string, any>>) => {
                 return req.originalUrl.replace(
                     '/modiacontextholder/api/decorator',
@@ -19,7 +19,7 @@ const setup = (app: Express) => {
                 options: RequestOptions,
                 req: Request<{}, any, any, ParsedQs, Record<string, any>>,
             ) => {
-                const accessToken = await requestOboToken(process.env.API_SCOPE!, req);
+                const accessToken = await requestOboToken(process.env.TILTAK_PROXY_API_SCOPE, req);
                 if (options?.headers) {
                     options.headers.Authorization = `Bearer ${accessToken}`;
                     let cookies = options.headers.cookie;
@@ -32,6 +32,20 @@ const setup = (app: Express) => {
                 const data = JSON.parse(proxyResData.toString('utf8'));
                 return JSON.stringify({ ...data, ident: data.identifikator || '' });
             },
+        }),
+    );
+
+    app.use(
+        '/modiacontextholder/redirect',
+        async (req, _, next) => {
+            const accessToken = await requestOboToken(process.env.MODIACONTEXTHOLDER_API_SCOPE, req);
+            req.headers.authorization = `Bearer ${accessToken}`;
+            next();
+        },
+        createProxyMiddleware({
+            target: 'http://modiacontextholder.personoversikt/modiacontextholder/redirect',
+            followRedirects: false,
+            changeOrigin: true,
         }),
     );
 
