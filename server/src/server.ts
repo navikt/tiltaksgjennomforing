05185509e49a8c27307cebd70834ec86d3b7c22b
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-09-30 11:02:20 +0200

    websocket-csp

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,6 @@ import * as appMedModiaDekoratoren from './dekorator/appMedModiaDekoratoren';
 import * as appMedNavDekoratoren from './dekorator/appMedNavDekoratoren';
 import { initializePath, BASEPATH, STATIC_PATHS } from './paths/setupPath';
 import { setupRoutes } from './routes';
-import { env } from 'process';
 
 const indexPath = path.resolve(__dirname, '../client', 'index.html');
 
@@ -38,7 +37,8 @@ if (ENABLE_EXTERNAL_MENU) {
             contentSecurityPolicy: {
                 useDefaults: true,
                 directives: {
-                    'default-src': ["'self'", ENV === 'dev' ? '*.intern.dev.nav.no' : '*.nav.no'],
+                    'default-src': ["'self'", '*.nav.no'],
+                    'connect-src': ["'self'", '*.nav.no'],
                     'script-src': ["'self'", '*.nav.no', '*.adeo.no', "'unsafe-inline'"],
                     'style-src': ["'self'", '*.nav.no', '*.adeo.no', "'unsafe-inline'"],
                     'font-src': ["'self'", '*.nav.no', 'data:'],
