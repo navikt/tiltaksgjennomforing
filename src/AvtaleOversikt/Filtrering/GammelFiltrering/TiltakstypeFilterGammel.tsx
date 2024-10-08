66d5a3e85c7db56c413b5518125f7dc173478f42
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2024-10-08 15:47:15 +0200

    fiks

--- src/AvtaleOversikt/Filtrering/GammelFiltrering/TiltakstypeFilterGammel.tsx
+++ src/AvtaleOversikt/Filtrering/GammelFiltrering/TiltakstypeFilterGammel.tsx
@@ -16,8 +16,8 @@ const TiltakstypeFilterGammel: FunctionComponent<FiltreringMedBeslutterProps> = 
         { value: 'MIDLERTIDIG_LONNSTILSKUDD', label: 'Midlertidig lønnstilskudd' },
         { value: 'VARIG_LONNSTILSKUDD', label: 'Varig lønnstilskudd' },
         { value: 'SOMMERJOBB', label: 'Sommerjobb' },
-        { value: 'VTAO', label: 'Varig tilrettelagt arbeid i ordinær virksomhet (VTA-O)' },
-    ];
+        vtaoTiltakToggle ? { value: 'VTAO', label: 'Varig tilrettelagt arbeid i ordinær virksomhet (VTA-O)' } : null,
+    ].filter((x) => x !== null) as OptionProps[];
 
     const alleTiltakstyper: OptionProps[] = [
         { value: '', label: 'Alle' },
