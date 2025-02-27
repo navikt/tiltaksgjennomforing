428dad034e55ca50b0798515f454779f73bbfc52
Hoang Mickel <mickel.hoang@bekk.no>
2025-02-27 15:52:47 +0100

    feat. varsel for adressesperre på kontaktinformasjon og godkjenning

---
+++ src/komponenter/Adressesperre/HemmeligAdresseVarsel.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Alert, Heading, Loader } from '@navikt/ds-react';
+import { useAvtaleKreverAktsomhet } from '@/services/use-rest';
+import { useParams } from 'react-router-dom';
+
+interface props {
+    avtaleId?: string;
+}
+
+const HemmeligAdresseVarsel = () => {
+    const { avtaleId } = useParams<{ avtaleId: string }>();
+    const { data: avtaleKreverAktsomhet } = useAvtaleKreverAktsomhet(avtaleId);
+
+    return avtaleKreverAktsomhet ? (
+        <Alert variant={'warning'}>
+            <Heading spacing size="small" level="3">
+                Hemmelig adresse
+            </Heading>
+            Denne personen har hemmelig adresse og du må derfor utvise aktsomhet.
+        </Alert>
+    ) : null;
+};
+export default HemmeligAdresseVarsel;
