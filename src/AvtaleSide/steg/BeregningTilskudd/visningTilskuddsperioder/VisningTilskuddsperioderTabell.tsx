854d39c993c65e81a39144b1b26931475f184f6b
Henrik uran <henrik.uran.nykmark@nav.no>
2025-03-12 14:02:50 +0100

    TilskuddsPeriodeTabell som en ordentlig tabell

--- src/AvtaleSide/steg/BeregningTilskudd/visningTilskuddsperioder/VisningTilskuddsperioderTabell.tsx
+++ src/AvtaleSide/steg/BeregningTilskudd/visningTilskuddsperioder/VisningTilskuddsperioderTabell.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { BodyShort, Label } from '@navikt/ds-react';
+import { Table } from '@navikt/ds-react';
 import { TilskuddsPeriode } from '@/types/avtale';
 import { formaterPeriode } from '@/utils/datoUtils';
 import EtikettStatus from '@/BeslutterSide/EtikettStatus';
@@ -24,55 +24,68 @@ const VisningTilskuddsperioderTabell: React.FC<Properties> = ({ className }: Pro
     const { startIndexVisning, sluttIndexVisning } = getIndexVisningForTilskuddsperiode(avtale, visAllePerioder);
     const cls = BEMHelper(className);
     return (
-        <div className={cls.element('tabell')}>
-            <div className={cls.element('tabell-ingress')}>
-                <Label>Tilskudd for perioder</Label>
-                {innloggetBruker.erNavAnsatt && <Label>Status</Label>}
-                <Label>Tilskuddsprosent</Label>
-                <Label>Inntil</Label>
-            </div>
-            {avtale.tilskuddPeriode
-                .filter((p: TilskuddsPeriode) => p.aktiv)
-                .map((periode: TilskuddsPeriode, index: number) => {
-                    const nyProsent: boolean =
-                        index > 0
-                            ? avtale.tilskuddPeriode[index - 1].lonnstilskuddProsent !== periode.lonnstilskuddProsent
-                            : false;
-                    if (index < startIndexVisning || index > sluttIndexVisning) {
-                        return null;
-                    } else if (index !== 0 && (index === startIndexVisning || index === sluttIndexVisning)) {
-                        return (
-                            <div key={index} className={cls.element('tabell-innslag')}>
-                                ...
-                            </div>
-                        );
-                    }
-                    return (
-                        <div
-                            key={index}
-                            className={cls.element('tabell-innslag')}
-                            style={{
-                                borderTop: nyProsent ? '2px solid gray' : 'undefined',
-                            }}
-                        >
-                            <BodyShort size="small">{formaterPeriode(periode.startDato, periode.sluttDato)}</BodyShort>
-                            {innloggetBruker.erNavAnsatt && (
-                                <BodyShort>
-                                    <EtikettStatus tilskuddsperiodestatus={periode.status} size="small" />
-                                </BodyShort>
-                            )}
-                            <BodyShort size="small">
-                                {avtale.tiltakstype === 'VARIG_LONNSTILSKUDD' &&
-                                    periode.status !== 'BEHANDLET_I_ARENA' && <>{periode.lonnstilskuddProsent}%</>}
-                                {(avtale.tiltakstype === 'MIDLERTIDIG_LONNSTILSKUDD' ||
-                                    avtale.tiltakstype === 'SOMMERJOBB') && <>{periode.lonnstilskuddProsent}%</>}
-                            </BodyShort>
-                            <BodyShort size="small" style={{ minWidth: '4rem' }}>
-                                {formaterPenger(periode.beløp)}
-                            </BodyShort>
-                        </div>
-                    );
-                })}
+        <>
+            <Table size={'medium'}>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell textSize={'small'}>Tilskudd for perioder</Table.HeaderCell>
+                        {innloggetBruker.erNavAnsatt && <Table.HeaderCell textSize={'small'}>Status</Table.HeaderCell>}
+                        <Table.HeaderCell textSize={'small'}>Tilskuddsprosent</Table.HeaderCell>
+                        <Table.HeaderCell textSize={'small'}>Inntil</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {avtale.tilskuddPeriode
+                        .filter((p: TilskuddsPeriode) => p.aktiv)
+                        .map((periode: TilskuddsPeriode, index: number) => {
+                            const nyProsent: boolean =
+                                index > 0
+                                    ? avtale.tilskuddPeriode[index - 1].lonnstilskuddProsent !==
+                                      periode.lonnstilskuddProsent
+                                    : false;
+                            if (index < startIndexVisning || index > sluttIndexVisning) {
+                                return null;
+                            } else if (index !== 0 && (index === startIndexVisning || index === sluttIndexVisning)) {
+                                return (
+                                    <Table.Row key={index}>
+                                        <Table.DataCell textSize={'small'} colSpan={100}>
+                                            ...
+                                        </Table.DataCell>
+                                    </Table.Row>
+                                );
+                            }
+                            return (
+                                <Table.Row
+                                    key={index}
+                                    style={{
+                                        borderTop: nyProsent ? '2px solid gray' : 'undefined',
+                                    }}
+                                >
+                                    <Table.DataCell textSize={'small'}>
+                                        {formaterPeriode(periode.startDato, periode.sluttDato)}
+                                    </Table.DataCell>
+                                    {innloggetBruker.erNavAnsatt && (
+                                        <Table.DataCell textSize={'small'}>
+                                            <EtikettStatus tilskuddsperiodestatus={periode.status} size="small" />
+                                        </Table.DataCell>
+                                    )}
+                                    <Table.DataCell align={'right'} textSize={'small'}>
+                                        {avtale.tiltakstype === 'VARIG_LONNSTILSKUDD' &&
+                                            periode.status !== 'BEHANDLET_I_ARENA' && (
+                                                <>{periode.lonnstilskuddProsent}%</>
+                                            )}
+                                        {(avtale.tiltakstype === 'MIDLERTIDIG_LONNSTILSKUDD' ||
+                                            avtale.tiltakstype === 'SOMMERJOBB') && (
+                                            <>{periode.lonnstilskuddProsent}%</>
+                                        )}
+                                    </Table.DataCell>
+                                    <Table.DataCell textSize={'small'}>{formaterPenger(periode.beløp)}</Table.DataCell>
+                                </Table.Row>
+                            );
+                        })}
+                </Table.Body>
+            </Table>
+
             <InfoRundtTilskuddsperioder
                 className={cls.className}
                 gjeldendeInnholdStartdato={avtale.gjeldendeInnhold.startDato}
@@ -81,7 +94,7 @@ const VisningTilskuddsperioderTabell: React.FC<Properties> = ({ className }: Pro
                 setVisAllePerioder={setVisAllePerioder}
                 visAllePerioder={visAllePerioder}
             />
-        </div>
+        </>
     );
 };
 export default VisningTilskuddsperioderTabell;
