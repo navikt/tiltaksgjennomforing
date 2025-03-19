f0eec35857971ac931cf07295c8927b14d0ef904
Hoang Mickel <mickel.hoang@bekk.no>
2025-03-19 09:53:08 +0100

    feat. lagt til kode 6 elr 7 i titeln til varsel

--- src/komponenter/Adressesperre/HemmeligAdresseVarsel.tsx
+++ src/komponenter/Adressesperre/HemmeligAdresseVarsel.tsx
@@ -5,6 +5,7 @@ import { useAvtaleKreverAktsomhet } from '@/services/use-rest';
 import { useInnloggetBruker } from '@/InnloggingBoundary/InnloggingBoundary';
 
 import { container } from './HemmeligAdresseVarsel.module.less';
+import { Diskresjonskode } from '@/types';
 
 interface props {
     avtaleId: string;
@@ -12,19 +13,32 @@ interface props {
 
 const ROLLER_SOM_KREVER_KONTROLL: Rolle[] = ['VEILEDER'];
 
+const kode6eller7 = (diskresjonskode?: Diskresjonskode) => {
+    switch (diskresjonskode) {
+        case Diskresjonskode.STRENGT_FORTROLIG_UTLAND:
+            return '- Kode 6';
+        case Diskresjonskode.STRENGT_FORTROLIG:
+            return '- Kode 6';
+        case Diskresjonskode.FORTROLIG:
+            return '- Kode 7';
+        default:
+            return '';
+    }
+};
+
 const HemmeligAdresseVarsel = ({ avtaleId }: props) => {
     const { rolle } = useInnloggetBruker();
     const isKreverKontroll = ROLLER_SOM_KREVER_KONTROLL.includes(rolle);
-    const { data: avtaleKreverAktsomhet } = useAvtaleKreverAktsomhet(isKreverKontroll ? avtaleId : undefined);
+    const { data: aktsomhet } = useAvtaleKreverAktsomhet(isKreverKontroll ? avtaleId : undefined);
 
-    if (!avtaleKreverAktsomhet) {
+    if (!aktsomhet || !aktsomhet.kreverAktsomhet) {
         return null;
     }
 
     return (
         <Alert variant={'warning'} className={container}>
             <Heading spacing size="small" level="3">
-                Hemmelig adresse
+                Hemmelig adresse {kode6eller7(aktsomhet.diskresjonskode)}
             </Heading>
             Denne personen har hemmelig adresse og du må derfor utvise aktsomhet.
         </Alert>
