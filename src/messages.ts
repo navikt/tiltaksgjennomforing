b69c5bc38773c576e9b14adfd4a8fec8d9783533
Eirik Vigeland <eirik.vigeland@nav.no>
2026-01-26 12:35:06 +0100

    fix. fikset mocking av avtaler etter at innholdType ble lagt til som felt i gjeldende innhold

--- src/messages.ts
+++ src/messages.ts
@@ -65,6 +65,7 @@ export const avtaleFelterBokmal: { [key in keyof Avtaleinnhold]: string } = {
     inkluderingstilskuddBegrunnelse: 'Begrunnelse for inkluderingstilskudd',
     inkluderingstilskuddTotalBeløp: 'Totalbeløp for inkluderingstilskudd',
     inkluderingstilskuddSats: 'Årlig gjeldende inkluderingstilskuddsats',
+    innholdType: 'INNGÅ',
 };
 
 export const inkluderingstilskuddtypeTekst: { [key in InkluderingstilskuddsutgiftType]: string } = {
