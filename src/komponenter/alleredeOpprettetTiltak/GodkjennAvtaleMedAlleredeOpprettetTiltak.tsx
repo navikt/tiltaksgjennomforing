4d92e4d473f84e13bf96375979b676fa7880a63c
Magnus Rom <magnus.skalleberg.rom@nav.no>
2026-04-14 16:03:00 +0200

    Lagt til LocalAlert

--- src/komponenter/alleredeOpprettetTiltak/GodkjennAvtaleMedAlleredeOpprettetTiltak.tsx
+++ src/komponenter/alleredeOpprettetTiltak/GodkjennAvtaleMedAlleredeOpprettetTiltak.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { AlleredeRegistrertAvtale } from '@/types/avtale';
 import InfoModal from '@/komponenter/modal/InfoModal';
-import { Alert, Heading } from '@navikt/ds-react';
+import { Alert, Heading, LocalAlert } from '@navikt/ds-react';
 import AlleredeOpprettetAvtale from './innholdsvisning/AlleredeOpprettetAvtale';
+import { Feilkode, Feilmeldinger } from '@/types/feilkode';
+import VerticalSpacer from '../layout/VerticalSpacer';
+import { FeilkodeError } from '@/types';
 
 interface Props {
     alleredeRegistrertAvtale: AlleredeRegistrertAvtale[] | [];
@@ -12,9 +16,22 @@ interface Props {
 
 const GodkjennMedAlleredeOpprettetTiltak = (props: Props) => {
     const { alleredeRegistrertAvtale, isApen, onLagre, onLukk } = props;
+    const [error, setError] = React.useState<FeilkodeError | undefined>();
+
+    const handleLagre = async () => {
+        try {
+            await onLagre();
+        } catch (err) {
+            if (err instanceof FeilkodeError) {
+                setError(err);
+            } else {
+                throw err;
+            }
+        }
+    };
 
     return (
-        <InfoModal width="medium" open={isApen} confirmText="Godkjenn avtale" onConfirm={onLagre} onClose={onLukk}>
+        <InfoModal width="medium" open={isApen} confirmText="Godkjenn avtale" onConfirm={handleLagre} onClose={onLukk}>
             <Heading size="medium" spacing id="Allerede registrerte tiltak for deltaker">
                 Godkjenning av avtale
             </Heading>
@@ -23,6 +40,16 @@ const GodkjennMedAlleredeOpprettetTiltak = (props: Props) => {
                 deltaker sitt fødselsnummer.
             </Alert>
             <AlleredeOpprettetAvtale alleredeRegistrertAvtale={alleredeRegistrertAvtale} />
+            {error?.message === 'OPPFOLGINGSTATUS_ENDRET' && (
+                <LocalAlert status="error" style={{ marginBottom: '2rem' }}>
+                    <LocalAlert.Header>
+                        <LocalAlert.Title>Avtalen må signeres på nytt</LocalAlert.Title>
+                    </LocalAlert.Header>
+                    <LocalAlert.Content>
+                        Deltakers innsatsbehov har endret seg, og avtalen må derfor signeres på nytt av alle parter.
+                    </LocalAlert.Content>
+                </LocalAlert>
+            )}
         </InfoModal>
     );
 };
