66d9a2b412cbc302712480460bd593900785f83d
Eirik Vigeland <eirik.vigeland@nav.no>
2026-09-07 14:13:02 +0200

    fix. Oppdater melding ved endret innsatsbehov (#1549)

--- src/AvtaleSide/steg/GodkjenningSteg/Godkjenning/godkjenningVeileder/GodkjennPaVegneAvDeltaker.tsx
+++ src/AvtaleSide/steg/GodkjenningSteg/Godkjenning/godkjenningVeileder/GodkjennPaVegneAvDeltaker.tsx
@@ -78,7 +78,7 @@ function GodkjennPaVegneAvDeltaker() {
             try {
                 await onLagre();
             } catch (err) {
-                if (err instanceof FeilkodeError && err.message === 'OPPFOLGINGSTATUS_ENDRET') {
+                if (err instanceof FeilkodeError && err.message === 'INNSATSGRUPPE_ENDRET') {
                     setInnsatsbehovVarselModalIsOpen(true);
                 } else {
                     throw err;
